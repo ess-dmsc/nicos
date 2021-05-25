de676caa22cdaae226dba537b048baac2ca74a4c
Ebad Kamil <ebad.kamil@ess.eu>
2021-05-25 10:02:00 +0200

    Use MultiLiveData panel in dream gui

--- nicos_ess/dream/guiconfig.py
+++ nicos_ess/dream/guiconfig.py
@@ -25,7 +25,7 @@ main_window = docked(
                      ('Scan Plot',
                       panel('nicos_ess.gui.panels.scans.ScansPanel')),
                      ('Detector Image',
-                      panel('nicos_ess.gui.panels.live.LiveDataPanel')),
+                      panel('nicos_ess.dream.gui.live.MultiLiveDataPanel')),
                      ('Script Status',
                       panel('nicos_ess.gui.panels.status.ScriptStatusPanel',
                             eta=True)),
@@ -50,7 +50,7 @@ main_window = docked(
                       tools=None),
             ), # vsplit
         ),
-        ('Detector Image', panel('nicos_ess.gui.panels.live.LiveDataPanel')),
+        ('Detector Image', panel('nicos_ess.dream.gui.live.MultiLiveDataPanel')),
         ('History',
             panel('nicos_ess.gui.panels.history.HistoryPanel'),
         ),
