9b1f79ed1483aa58537d33d17f5dd31aec62c9cd
Ebad Kamil <ebad.kamil@ess.eu>
2021-06-14 15:16:34 +0200

    Add difference curve in PlotWidget

--- nicos_ess/dream/gui/comparison_window.py
+++ nicos_ess/dream/gui/comparison_window.py
@@ -1,31 +1,39 @@
 import numpy as np
 
+from nicos.guisupport.plots import MaskedPlotCurve
 from nicos.guisupport.qt import QHBoxLayout, QMainWindow, QPushButton, \
     QVBoxLayout, QWidget, pyqtSlot
 
-from nicos_mlz.toftof.gui.resolutionpanel import PlotWidget as BasePlotWidget
+from nicos_mlz.toftof.gui.resolutionpanel import COLOR_GREEN, PlotWidget as BasePlotWidget
 
 
 class PlotWidget(BasePlotWidget):
-    def setData(self, x1, y1, x2=None, y2=None):
-        for curve, (x, y) in zip(self.plot._curves, ((x1, y1), (x2, y2))):
+
+    def setData(self, x1, y1, x2=None, y2=None, difference=False):
+        for curve, (x, y) in zip(self.plot._curves[:-1], ((x1, y1), (x2, y2))):
             if y is not None:
                 curve.x = np.array(x)
                 curve.y = np.array(y)
+        if difference:
+            self.plot._curves[-1].x = np.array(x1)
+            self.plot._curves[-1].y = np.array(y1) - np.array(y2)
 
         self.plot.reset()
         self.plot.update()
 
-    def resetBackground(self):
-        self.plot._curves[1].x = np.array([0])
-        self.plot._curves[1].y = np.array([0])
-        self.plot.reset()
-        self.plot.update()
+    def reset_background(self):
+        for curve in self.plot._curves[1:]:
+            curve.x = np.array([0])
+            curve.y = np.array([0])
 
 
 class ComparisonPlot(PlotWidget):
     def __init__(self, parent):
         PlotWidget.__init__(self, "Comparison Plot", "x", "y", 2, parent=parent)
+        curve = MaskedPlotCurve(
+            [0], [1], linewidth=2, legend='Difference', linecolor=COLOR_GREEN)
+        self.plot.axes.addCurves(curve)
+        self.plot._curves.append(curve)
         self.plot._curves[0].legend = "Live"
         self.plot._curves[1].legend = "Background"
 
@@ -40,6 +48,7 @@ class ComparisonWindow(QMainWindow):
         if parent is not None:
             parent.registerWindow(self)
 
+        self.background_data = None
         self._central_widget = QWidget()
         self._test = QWidget()
 
@@ -74,14 +83,17 @@ class ComparisonWindow(QMainWindow):
     def setData(self, live_data_blob, background_data_blob=None):
         if not live_data_blob and not background_data_blob:
             return
+        if background_data_blob:
+            self.background_data = background_data_blob
 
         x1, y1, x2, y2 = None, None, None, None
         if live_data_blob:
             x1, y1 = self._extract_data(live_data_blob)
-        if background_data_blob:
-            x2, y2 = self._extract_data(background_data_blob)
 
-        self._plot.setData(x1, y1, x2, y2)
+        if self.background_data:
+            x2, y2 = self._extract_data(self.background_data)
+
+        self._plot.setData(x1, y1, x2, y2, difference=np.array_equal(x1, x2))
 
     def _extract_data(self, blob):
         labels, data = blob
@@ -93,7 +105,8 @@ class ComparisonWindow(QMainWindow):
 
     @pyqtSlot()
     def on_resetBackgroundButton_clicked(self):
-        self._plot.resetBackground()
+        self.background_data = ({}, np.array([0]))
+        self._plot.reset_background()
 
     def closeEvent(self, QCloseEvent):
         if self.parent() is not None:
