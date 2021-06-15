cee7ea232ab779d24af1bd25b861d882f5a1abd8
Ebad Kamil <ebad.kamil@ess.eu>
2021-06-15 14:06:00 +0200

    Reset differnce curve to clean

--- nicos_ess/dream/gui/comparison_window.py
+++ nicos_ess/dream/gui/comparison_window.py
@@ -1,3 +1,27 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2021 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Ebad Kamil <ebad.kamil@ess.eu>
+#
+# *****************************************************************************
+
 import numpy as np
 
 from nicos.guisupport.livewidget import IntegralLiveWidget
@@ -17,6 +41,9 @@ class PlotWidget1D(PlotWidget):
         if difference:
             self.plot._curves[-1].x = np.array(x1)
             self.plot._curves[-1].y = np.array(y1) - np.array(y2)
+        else:
+            self.plot._curves[-1].x = np.array([0])
+            self.plot._curves[-1].y = np.array([0])
 
         self.plot.reset()
         self.plot.update()
--- nicos_ess/dream/gui/live.py
+++ nicos_ess/dream/gui/live.py
@@ -1,3 +1,27 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2021 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Ebad Kamil <ebad.kamil@ess.eu>
+#
+# *****************************************************************************
+
 import os.path as osp
 from datetime import datetime
 from os import path
@@ -52,6 +76,18 @@ class LiveDataPanel(DefaultLiveDataPanel):
         self._set_background_data()
         return self._compare_window
 
+    @pyqtSlot()
+    def on_snapShotButton_clicked(self):
+        data, labels = self._extract_data()
+        if not data:
+            return
+        uid = uuid4()
+        self._snap_cache[uid] = {}
+        self._snap_cache[uid]['dataarrays'] = data
+        self._snap_cache[uid]['labels'] = labels
+        self.add_to_flist(
+            f'snapshot_{datetime.now()}', '', SNAP, uid)
+
     def _set_background_data(self):
         # Deal with 1D data only for the timebeing
         data = self._get_1d_data()
@@ -69,10 +105,6 @@ class LiveDataPanel(DefaultLiveDataPanel):
         if data and len(data[0].shape) == 1:
             return labels, data[0]
         return
-
-    def on_fileList_currentItemChanged(self):
-        DefaultLiveDataPanel.on_fileList_currentItemChanged(self)
-        self.on_live_data_update()
 
     @pyqtSlot()
     def on_actionSaveData_triggered(self):
@@ -116,35 +148,6 @@ class LiveDataPanel(DefaultLiveDataPanel):
         labels = current_blob.get("labels", {})
         return data, labels
 
-    def registerWindow(self, instance):
-        """Register an instance of a QMainWindow"""
-        self._registered_windows[instance] = 1
-
-    def unregisterWindow(self, instance):
-        """De-Register an instance if closeEvent is called"""
-        if instance in self._registered_windows:
-            del self._registered_windows[instance]
-
-    def checkWindowOpen(self, instance, windows):
-        """Check if window is already open, then bring it to front"""
-        for window in windows:
-            if isinstance(window, instance):
-                window.activateWindow()
-                return True
-        return False
-
-    @pyqtSlot()
-    def on_snapShotButton_clicked(self):
-        data, labels = self._extract_data()
-        if not data:
-            return
-        uid = uuid4()
-        self._snap_cache[uid] = {}
-        self._snap_cache[uid]['dataarrays'] = data
-        self._snap_cache[uid]['labels'] = labels
-        self.add_to_flist(
-            f'snapshot_{datetime.now()}', '', SNAP, uid)
-
     def getDataFromItem(self, item):
         if item is None:
             return
@@ -167,3 +170,24 @@ class LiveDataPanel(DefaultLiveDataPanel):
                     item.setData(FILEUID, None)
                 else:
                     self.fileList.takeItem(index)
+
+    def on_fileList_currentItemChanged(self):
+        DefaultLiveDataPanel.on_fileList_currentItemChanged(self)
+        self.on_live_data_update()
+
+    def registerWindow(self, instance):
+        """Register an instance of a QMainWindow"""
+        self._registered_windows[instance] = 1
+
+    def unregisterWindow(self, instance):
+        """De-Register an instance if closeEvent is called"""
+        if instance in self._registered_windows:
+            del self._registered_windows[instance]
+
+    def checkWindowOpen(self, instance, windows):
+        """Check if window is already open, then bring it to front"""
+        for window in windows:
+            if isinstance(window, instance):
+                window.activateWindow()
+                return True
+        return False
