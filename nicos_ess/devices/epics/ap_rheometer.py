70443f2aabddc85f80da42c580ed11bf328a544a
Jonas Petersson <jonas.petersson@ess.eu>
2024-01-03 09:26:41 +0100

    tested and works

--- nicos_ess/devices/epics/ap_rheometer.py
+++ nicos_ess/devices/epics/ap_rheometer.py
@@ -22,6 +22,8 @@
 #   Jonas Petersson <jonas.petersson@ess.eu>
 #
 # *****************************************************************************
+import time
+
 from nicos.core import SIMULATION, Override, Param, pvname, status
 from nicos.devices.abstract import MappedMoveable
 
@@ -62,9 +64,8 @@ class RheometerControl(EpicsDevice, MappedMoveable):
 
     def _set_custom_record_fields(self):
         self._record_fields['set_config'] = 'LoadMeasConfig-S.VAL$'
-        self._record_fields['update_config'] = 'LoadMeasConfig.PROC'
-        self._record_fields['read_config'] = 'LoadMeasConfig'
-        self._record_fields['start'] = 'StartMeasurement'
+        self._record_fields['read_config'] = 'LoadMeasConfig-RB'
+        self._record_fields['start'] = 'Start'
         self._record_fields['proc_start'] = 'MeasHeader-R.PROC'
 
     def _get_pv_parameters(self):
@@ -84,13 +85,13 @@ class RheometerControl(EpicsDevice, MappedMoveable):
 
     def doStop(self):
         pass
-        # self._put_pv(f"{self.pv_root}Stop.Proc", 1)  # Is this the right PV?
 
     def doStart(self, target=None):
         CONFIG = ':PROG["Test",TEST[(PART[(NUMB[5,LAST],DTIM[1,1,REL]),(),(),(),(SRAT[1,FUNC[LOG,(1,10)]]),(),(),,(DAPT[TEMP[2,??T]],DAPT[TORQ[1,??T]],DAPT[SPEE[1,??T]],DAPT[EXCU[1,??T]],DAPT[FORC[1,??T]],DAPT[VOLT[1,??T]],DAPT[DIST[1,??T]],GSTR[STAT[1,??T]],DAPT[VELO[1,??T]],DAPT[DGAP[1,??T]],DAPT[TIMA[1,??T]],DAPT[TIMP[1,??T]],DAPT[EXCE[1,??T]],DAPT[ETRQ[1,??T]]),(GENP[0,(IFDT[EX])],SETV[0,(IFST[IN,(16)])]),(EXCU[1,!?])],PART[(NUMB[10,LAST],DTIM[FUNC[LIN,(1,1.888888889)],2,REL]),(),(),(),(STRA[1,OSCI[FUNC[LIN,(0.03,0.07)],FUNC[LOG,(0.3183098862,1.432394488)],SIN]]),(),(),VALF[STRA[1,?&]],(VALF[STRA[1,?&]],DAPT[TEMP[2,??T]],COMP[MODU[1,??F],1,PHAS],COMP[TORQ[1,??F],0,CABS],COMP[TORQ[1,??F],1,CABS],DAPT[KFAC[1,??T]],COMP[SPEE[1,??F],0,CABS],COMP[EXCU[1,??F],0,CABS],COMP[EXCU[1,??F],1,CABS],COMP[FORC[1,??F],0,REAL],DAPT[VOLT[1,??T]],DAPT[DIST[1,??T]],GSTR[STAT[1,??T]],DAPT[VELO[1,??T]],DAPT[DGAP[1,??T]],DAPT[TIMA[1,??T]],DAPT[TIMP[1,??T]],COMP[EXCE[1,??F],0,CABS],COMP[EXCE[1,??F],1,CABS],COMP[ETRQ[1,??F],0,CABS],COMP[ETRQ[1,??F],1,CABS]),(GENP[0,(IFDT[EX])],SETV[0,(IFST[IN,(16)])]),()],PART[(NUMB[12,LAST]),(),(),(),(SRAT[1,FUNC[LIN,(1,5)]]),(),(),,(DAPT[TEMP[2,??T]],DAPT[TORQ[1,??T]],DAPT[SPEE[1,??T]],DAPT[EXCU[1,??T]],DAPT[FORC[1,??T]],DAPT[VOLT[1,??T]],DAPT[DIST[1,??T]],GSTR[STAT[1,??T]],DAPT[VELO[1,??T]],DAPT[DGAP[1,??T]],DAPT[TIMA[1,??T]],DAPT[TIMP[1,??T]],DAPT[EXCE[1,??T]],DAPT[ETRQ[1,??T]]),(GENP[0,(IFST[IN,(17)])],SETV[0,(IFST[IN,(16)])]),()])],EXIT[()],CANC[()]]'
         conf_list_with_q = [*CONFIG]
         conf_list_ascii = [ord(c) for c in conf_list_with_q]
         self._put_pv('set_config', conf_list_ascii)
-        self._put_pv('update_config', 1)
+        time.sleep(2)
         self._put_pv('proc_start', 1)
+        time.sleep(1)
         self._put_pv('start', 1)
