575507d53bd329db9ec4e3dd3a915b856d9c4764
Matt Clarke <matt.clarke@ess.eu>
2024-06-18 08:22:28 +0200

    Ignore updates if not OP_TELL

--- nicos_ess/devices/cache_kafka_forwarder.py
+++ nicos_ess/devices/cache_kafka_forwarder.py
@@ -29,9 +29,10 @@ from streaming_data_types.alarm_al00 import Severity, serialise_al00
 from streaming_data_types.logdata_f144 import serialise_f144
 
 from nicos.core import Device, Override, Param, host, listof, status
-from nicos.protocols.cache import cache_load
+from nicos.protocols.cache import OP_TELL, cache_load
 from nicos.services.collector import ForwarderBase
 from nicos.utils import createThread
+
 from nicos_ess.devices.kafka.producer import KafkaProducer
 
 nicos_status_to_al00 = {
@@ -145,7 +146,7 @@ class CacheKafkaForwarder(ForwarderBase, Device):
         return False
 
     def _putChange(self, timestamp, ttl, key, op, value):
-        if value is None:
+        if value is None or op != OP_TELL:
             return
         dev_name = key[0 : key.index("/")]
         if not self._checkKey(key) or not self._checkDevice(dev_name):
