2197eb2dce0bcb6da17ceb6f0b7ed55ee93d61ca
Josephine <josephine.hojding@ess.eu>
2024-08-09 10:06:16 +0200

    test

--- nicos_ess/commands/default_setup.py
+++ nicos_ess/commands/default_setup.py
@@ -4,16 +4,8 @@ from nicos.commands import usercommand
 
 @usercommand
 def default_setup():
-    """system_devs = (
-        session.configured_devices if "system" in session.loaded_setups else {}
-    )"""
-
-    # session.log.info(f"loaded setups: {session.loaded_setups}")
-    for d in session._setup_info["system"]["devices"]:
-        session.log.info(f"{d}")
-    # session.log.info(f"setup info: {session._setup_info['system']['devices']}")
-
-    """for devname, dev in session.devices.items():
+    system_devs = session._setup_info["system"]["devices"]
+    for devname, dev in session.devices.items():
         if devname not in system_devs:
             session.log.info(f"non system dev: {dev}")
-        session.log.info(f"system dev: {dev}")"""
+        session.log.info(f"system dev: {dev}")
