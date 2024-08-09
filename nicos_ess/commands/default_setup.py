b3bd3af2530c9163af18885336fcf33fbc369b1d
Josephine <josephine.hojding@ess.eu>
2024-08-09 09:40:40 +0200

    test

--- nicos_ess/commands/default_setup.py
+++ nicos_ess/commands/default_setup.py
@@ -8,9 +8,10 @@ def default_setup():
         session.configured_devices if "system" in session.loaded_setups else {}
     )
 
+    session.log.info(f"loaded setups: {session.loaded_setups}")
+    session.log.info(f"setup info: {session._setup_info}")
+
     for devname, dev in session.devices.items():
-        session.log.info(f"loaded setups: {session.loaded_setups}")
-        session.log.info(f"setup info: {session._setup_info}")
         if devname not in system_devs:
             session.log.info(f"non system dev: {dev}")
         session.log.info(f"system dev: {dev}")
