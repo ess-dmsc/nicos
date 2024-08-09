c97a273f9088777ae0ec350d4eb9e94b222c4556
Josephine <josephine.hojding@ess.eu>
2024-08-09 11:25:47 +0200

    test

--- nicos_ess/commands/default_setup.py
+++ nicos_ess/commands/default_setup.py
@@ -3,12 +3,13 @@ from nicos.commands import usercommand
 
 
 @usercommand
-def default_setup():
-    system_devs = session._setup_info["system"]["devices"]
-
-    loaded_devices = {}
-    for devname, dev in session.devices.items():
-        if devname not in system_devs:
-            loaded_devices[devname] = dev
-
-    session.log.info(loaded_devices)
+def default_setup(*args):
+    loaded_devices = {
+        devname: dev
+        for devname, dev in session.devices.items()
+        if devname not in session._setup_info["system"]["devices"]
+    }
+    for device in args:
+        loaded_devices.get(
+            device, lambda: None
+        ).device_default()  ## Implement device_default() for devices?
