f9850d1531f1b70d2a555dcc4bdc38340cedbcd9
Jonas Petersson <jonas.petersson@ess.eu>
2024-08-21 14:06:56 +0000

    Add rules for ruff

--- nicos_ess/coda/setups/forwarder.py
+++ nicos_ess/coda/setups/forwarder.py
@@ -1,7 +1,10 @@
-description = 'Monitors the status of the Forwarder'
+description = "Monitors the status of the Forwarder"
 
-devices = dict(KafkaForwarder=device(
-    'nicos_ess.devices.forwarder.EpicsKafkaForwarder',
-    description='Monitors the status of the Forwarder',
-    statustopic='coda_forwarder_status',
-    brokers=['10.100.1.19:8093']), )
+devices = dict(
+    KafkaForwarder=device(
+        "nicos_ess.devices.forwarder.EpicsKafkaForwarder",
+        description="Monitors the status of the Forwarder",
+        statustopic="coda_forwarder_status",
+        brokers=["10.100.1.19:8093"],
+    ),
+)
