5855dedc787cb3ddf7b5f34443708e4d8dd3611c
Jonas Petersson <jonas.petersson@ess.eu>
2024-10-09 07:20:26 +0000

    loki thermobox

---
+++ nicos_ess/nido/setups/loki_thermo_box.py
@@ -0,0 +1,17 @@
+description = "The temperature readout box for LOKI."
+
+pv_root = "LOKI:SmplTmp-IOC-001:"
+
+devices = dict()
+
+num_cells = 8
+for i in range(1, num_cells + 1):
+    devices[f"s_temp_{i}"] = device(
+        "nicos_ess.devices.epics.pva.EpicsReadable",
+        description=f"Temperature sensor {i}.",
+        readpv=f"{pv_root}PT100_TS{i}",
+        pva=True,
+        monitor=True,
+        pollinterval=None,
+        maxage=None,
+    )
