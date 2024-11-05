294f2b450de2b9fe167efbb3b909dcb578fbbefa
linem <line.moller@gmail.com>
2024-11-05 16:04:27 +0100

    checkpoint

--- nicos_ess/gui/lkm_sample_panel_gui_dev.py
+++ nicos_ess/gui/lkm_sample_panel_gui_dev.py
@@ -19,12 +19,9 @@ from PyQt6.QtWidgets import (
 
 
 class Sample:
-    def __init__(self, sample_id, annotation_string):
+    def __init__(self, sample_id, annotations):
         self.sample_id = sample_id
-        self.annotations = OrderedDict()
-        self.annotations["annotation_string"] = annotation_string  # None
-        self.annotations["annotation_int"] = 2  # None
-        self.annotations["annotation_float"] = 0.5  # None
+        self.annotations = annotations  # ordered dict
 
     def add_sample_annotation(self, key, value):
         self.annotations[key] = value
@@ -39,8 +36,8 @@ class Proposal:
         self.samples = {}
         self.sample_annotation_keys = []
 
-    def add_sample(self, sample_id, annotation_string):
-        new_sample = Sample(sample_id, annotation_string)
+    def add_sample(self, sample_id, annotations):
+        new_sample = Sample(sample_id, annotations)
         self.samples[sample_id] = new_sample
         self.add_annotation_keys(new_sample.annotations.keys())
 
@@ -48,6 +45,35 @@ class Proposal:
         for key in keys:
             if key not in self.sample_annotation_keys:
                 self.sample_annotation_keys.append(key)
+
+
+def get_a_test_proposal():
+    a_test_proposal = Proposal(proposal_id="123")
+    a_test_proposal.add_sample(
+        "Sample1",
+        OrderedDict(
+            {"annot_string": "InfoA", "annot_int": 1, "annot_float": 0.5}
+        ),
+    )
+    a_test_proposal.add_sample(
+        "Sample2",
+        OrderedDict(
+            {"annot_string": "InfoB", "annot_int": 2, "annot_float": 0.4}
+        ),
+    )
+    a_test_proposal.add_sample(
+        "Sample3",
+        OrderedDict(
+            {"annot_string": "InfoC", "annot_int": 3, "annot_float": 0.3}
+        ),
+    )
+    a_test_proposal.add_sample(
+        "Sample4",
+        OrderedDict(
+            {"annot_string": "InfoD", "annot_int": 4, "annot_float": 0.2}
+        ),
+    )
+    return a_test_proposal
 
 
 class MainWindow(QMainWindow):
@@ -69,11 +95,7 @@ class SamplePanel(QWidget):  # QWidget
     def __init__(self):
         super().__init__(parent=None)
 
-        self.proposal = Proposal(proposal_id="123")
-        self.proposal.add_sample("Sample1", "InfoA")
-        self.proposal.add_sample("Sample2", "InfoB")
-        self.proposal.add_sample("Sample3", "InfoC")
-        self.proposal.add_sample("Sample4", "InfoD")
+        self.proposal = get_a_test_proposal()
 
         # sample main layout
         self.sample_main_layout = QVBoxLayout()
@@ -99,25 +121,27 @@ class SamplePanel(QWidget):  # QWidget
         self.sample_info_main_layout = QVBoxLayout()
 
         # annotation row for sample id
-        self.sample_id_row_layout = QHBoxLayout()
-        self.sample_id_key_label = QLabel("Sample ID")
-        self.sample_id_value_label = QLabel()
-        self.sample_id_edit_field = QLineEdit()
-        self.sample_id_edit_field.hide()
-        self.sample_id_row_layout.addWidget(self.sample_id_key_label)
-        self.sample_id_row_layout.addWidget(self.sample_id_value_label)
+        self.sample_id_widgets_layout = QHBoxLayout()
+        self.sample_id_widgets_list = [
+            QLabel("Sample ID"),
+            QLabel(),
+            QLineEdit(),
+        ]
+        for i, widget in enumerate(self.sample_id_widgets_list):
+            self.sample_id_widgets_layout.addWidget(
+                widget, alignment=Qt.AlignmentFlag.AlignLeft
+            )
 
         # annotation rows for all other annotations
         self.sample_info_widgets_layout = QGridLayout()
         self.sample_info_widgets_list = []
         for i, annotation in enumerate(self.proposal.sample_annotation_keys):
-            row = [QLabel(annotation), QLabel("placeholder"), QLineEdit("placeholder")]
+            row = [QLabel(annotation), QLabel(), QLineEdit()]
             self.sample_info_widgets_list.append(row)
             for j, widget in enumerate(row):
-                self.sample_info_widgets_layout.addWidget(widget, i, j)
-                if j == 2:
-                    # hide the widgets for editing
-                    widget.hide()
+                self.sample_info_widgets_layout.addWidget(
+                    widget, i, j, alignment=Qt.AlignmentFlag.AlignLeft
+                )
 
         # control buttons for add and editing samples
         self.sample_info_button_row_layout = QHBoxLayout()
@@ -138,13 +162,17 @@ class SamplePanel(QWidget):  # QWidget
         self.sample_info_button_row_layout.addWidget(
             self.button_save, alignment=Qt.AlignmentFlag.AlignRight
         )
-        self.sample_info_button_row_widget.setLayout(self.sample_info_button_row_layout)
+        self.sample_info_button_row_widget.setLayout(
+            self.sample_info_button_row_layout
+        )
         self.sample_info_button_row_widget.hide()
 
         # combine widgets and layouts
-        self.sample_info_main_layout.addLayout(self.sample_id_row_layout)
+        self.sample_info_main_layout.addLayout(self.sample_id_widgets_layout)
         self.sample_info_main_layout.addLayout(self.sample_info_widgets_layout)
-        self.sample_info_main_layout.addWidget(self.sample_info_button_row_widget)
+        self.sample_info_main_layout.addWidget(
+            self.sample_info_button_row_widget
+        )
         self.sample_info_main_layout.addStretch()
         self.sample_info_main_widget.setLayout(self.sample_info_main_layout)
 
@@ -154,62 +182,135 @@ class SamplePanel(QWidget):  # QWidget
         self.sample_main_layout.addLayout(self.top_button_row_layout)
         self.sample_main_layout.addWidget(self.panel_splitter)
 
-        #
-        # # populate sample list
-        # self.sample_selector.addItems(self.proposal.samples.keys())
-        # self.sample_selector.itemClicked.connect(self.display_sample_info)
-        #
-        # # buttons for save and cancel adding and editing
-
-    def display_sample_info(self, item):
-        sample = self.proposal.samples[item.text()]
-        for i, (key, val) in enumerate(sample.annotations.items()):
-            self.sample_field_widgets[i][0].setText(key)
-            self.sample_field_widgets[i][1].setText(str(val))
-
-    def add_sample(self):
-        for i, key in enumerate(self.proposal.sample_annotation_keys):
-            self.sample_field_widgets[i][0].setText(key)
-            self.sample_field_widgets[i][1].hide()
-            self.sample_field_widgets[i][2].show()
-        self.sample_selector.setDisabled(True)
-        self.sample_info_button_layout = QHBoxLayout()
-        self.sample_info_button_layout.addWidget(
-            self.button_new_field, alignment=Qt.AlignmentFlag.AlignLeft
+        # populate sample list
+        self.sample_selector_widget.addItems(self.proposal.samples.keys())
+        self.sample_selector_widget.itemClicked.connect(
+            self.display_sample_info
         )
-        self.sample_info_button_layout.addWidget(
-            self.button_cancel, alignment=Qt.AlignmentFlag.AlignRight
-        )
-        self.sample_info_button_layout.addWidget(
-            self.button_save, alignment=Qt.AlignmentFlag.AlignRight
-        )
-        self.sample_field_layout.addLayout(self.sample_info_button_layout)
 
-    def edit_sample(self):
-        for i, widget_row in enumerate(self.sample_info_widgets_list):
-            for j, widget in enumerate(widget_row):
-                if j == 1:
-                    widget.hide()
+        self.status_view()
+
+    def status_view(self):
+        #  add button enabled
+        self.button_add_sample.setEnabled(True)
+        #  edit button disabled
+        self.button_edit_sample.setDisabled(True)
+        #  sample selector enabled / none selected
+        self.sample_selector_widget.setEnabled(True)
+        self.sample_selector_widget.clearSelection()
+        #  sample ID view mode
+        self.sample_id_widgets_list[0].show()
+        self.sample_id_widgets_list[1].show()
+        self.sample_id_widgets_list[2].hide()
+        #  sample info view mode
+        for i, row in enumerate(self.sample_info_widgets_list):
+            for j, widget in enumerate(row):
                 if j == 2:
-                    # hide the widgets for editing
-                    widget.show()
-        self.sample_info_button_row_widget.show()
-
-    def add_info_field(self):
-        print("not implemented")
-
-    def cancel_add_or_edit(self):
-        for i, widget_row in enumerate(self.sample_info_widgets_list):
-            for j, widget in enumerate(widget_row):
-                if j == 1:
-                    widget.show()
-                if j == 2:
-                    # hide the widgets for editing
                     widget.hide()
+                else:
+                    widget.show()
+        #  add field, cancel, save button hidden
         self.sample_info_button_row_widget.hide()
 
+    def left_panel_disabled(self):
+        self.button_add_sample.setDisabled(True)
+        self.button_edit_sample.setDisabled(True)
+        self.sample_selector_widget.setDisabled(True)
+
+    def display_sample_info(self, item):
+        if item is not None:
+            # status
+            #  add button enabled
+            #  edit button enabled
+            #  sample selector enabled
+            #  sample ID view mode
+            #  sample info view mode
+            #  add field button hidden
+            #  cancel, save button hidden
+            sample_id = item.text()
+            self.sample_id_widgets_list[1].setText(sample_id)
+            sample = self.proposal.samples[sample_id]
+            for i, (key, val) in enumerate(sample.annotations.items()):
+                self.sample_info_widgets_list[i][0].setText(key)
+                self.sample_info_widgets_list[i][1].setText(str(val))
+            self.button_edit_sample.setEnabled(True)
+        else:
+            # triggered from cancel button
+            self.sample_id_widgets_list[1].setText("")
+            for i in range(len(self.sample_info_widgets_list)):
+                self.sample_info_widgets_list[i][1].setText("")
+
+    def add_sample(self):
+        # status
+        #  add button disabled
+        #  edit button disabled
+        #  sample selector disabled
+        #  sample ID edit mode
+        #  sample info edit mode
+        #  add field button shown
+        #  cancel, save button shown
+        self.sample_id_widgets_list[1].hide()
+        self.sample_id_widgets_list[2].show()
+        for i, key in enumerate(self.proposal.sample_annotation_keys):
+            self.sample_info_widgets_list[i][0].setText(key)
+            self.sample_info_widgets_list[i][1].hide()
+            self.sample_info_widgets_list[i][2].show()
+        self.sample_info_button_row_widget.show()
+        self.left_panel_disabled()
+
+    def edit_sample(self):
+        # status
+        #  add button disabled
+        #  edit button disabled
+        #  sample selector disabled
+        #  sample ID view mode
+        #  sample info edit mode
+        #  add field button shown
+        #  cancel, save button shown
+        sample_id = self.sample_selector_widget.currentItem().text()
+        sample = self.proposal.samples[sample_id]
+        for i, (key, val) in enumerate(sample.annotations.items()):
+            self.sample_info_widgets_list[i][1].hide()
+            self.sample_info_widgets_list[i][2].show()
+            self.sample_info_widgets_list[i][2].setText(str(val))
+        self.sample_info_button_row_widget.show()
+        self.left_panel_disabled()
+
+    def add_info_field(self):
+        # go to start status
+        n_rows = len(self.sample_info_widgets_list)
+        new_widgets = [QLineEdit(), QLabel(), QLineEdit()]
+        self.sample_info_widgets_list.append(new_widgets)
+        for j, widget in enumerate(new_widgets):
+            self.sample_info_widgets_layout.addWidget(
+                widget, n_rows + 1, j, alignment=Qt.AlignmentFlag.AlignLeft
+            )
+            if j == 1:
+                widget.hide()
+
+    def cancel_add_or_edit(self):
+        self.sample_id_widgets_list[2].setText("")
+        remove_empty_fields = []
+        for i, row in enumerate(self.sample_info_widgets_list):
+            for j, widget in enumerate(row):
+                if j == 0:
+                    if widget.text() == "":
+                        remove_empty_fields.append(i)
+                if j == 2:
+                    widget.setText("")
+        if len(remove_empty_fields) > 0:
+            for index in remove_empty_fields:
+                print(index)
+                print(len(self.sample_info_widgets_list))
+                del self.sample_info_widgets_list[index]
+                print(len(self.sample_info_widgets_list))
+        self.status_view()
+        self.display_sample_info(None)
+
     def save_add_or_edit(self):
+        # go to start status
         print("not implemented")
+        self.status_view()
 
 
 if __name__ == "__main__":
