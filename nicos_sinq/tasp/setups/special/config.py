9b63d175526069d411b05251d0a04dea769646dd
Matt Clarke <matt.clarke@ess.eu>
2024-12-18 14:30:30 +0100

    remove sinq stuff

--- nicos_sinq/tasp/setups/special/config.py
+++
@@ -1,9 +0,0 @@
-description = "Generic configuration settings for TASP"
-
-import os
-
-group = "configdata"
-
-DATA_PATH = os.environ.get("NICOSDUMP", ".") + "/tasp/"
-
-KAFKA_BROKERS = ["localhost:9092"]
