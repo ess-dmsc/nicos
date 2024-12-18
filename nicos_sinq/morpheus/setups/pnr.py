07dd0d54b41f25bf21d045cd9066aa075545a6d0
Matt Clarke <matt.clarke@ess.eu>
2024-12-18 13:54:52 +0000

    Remove sinq stuff

--- nicos_sinq/morpheus/setups/pnr.py
+++
@@ -1,7 +0,0 @@
-description = "Polarized Neutron Reflectivity on MORPHEUS"
-
-group = "basic"
-
-includes = ["morpheus", "magnets", "slits", "polarizer", "monochromator"]
-
-excludes = ["a34_aliases", "diffraction", "tas"]
--- nicos_sinq/neutra/setups/special/config.py
+++
@@ -1,9 +0,0 @@
-description = "Generic configuration settings for NEUTRA"
-
-import os
-
-group = "configdata"
-
-DATA_PATH = os.environ.get("NICOSDUMP", ".") + "/neutra/"
-
-KAFKA_BROKERS = ["localhost:9092"]
