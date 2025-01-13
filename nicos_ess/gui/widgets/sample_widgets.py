2877e4b1f8f0f0d3bcd2f8264b68531a3b908eb7
linemoller <line.moller@ess.eu>
2025-01-13 10:08:49 +0100

    make table selection to only one selected cell, add buttons for add row and delete row

--- nicos_ess/gui/widgets/sample_widgets.py
+++ nicos_ess/gui/widgets/sample_widgets.py
@@ -12,10 +12,17 @@ from nicos.guisupport.qt import (
     QWidget,
 )
 
+from PyQt5.QtWidgets import QAbstractItemView
+
 
 class SamplePanelWidgets(QWidget):
     def __init__(self):
         QWidget.__init__(self)
+
+        self.add_row_btn = None
+        self.delete_row_btn = None
+        self.add_dialog = None
+        self.remove_dialog = None
 
         self.top_add_remove_btn_layout = QHBoxLayout()
         self.btn_add = QPushButton("Add sample")
@@ -26,22 +33,29 @@ class SamplePanelWidgets(QWidget):
 
         self.side_edit_btn_layout = QVBoxLayout()
         self.btn_edit = QPushButton("Edit sample")
-        self.btn_custom = QPushButton("Customize properties")
+        self.btn_custom = QPushButton("Customise properties")
         self.btn_save = QPushButton("Save")
         self.btn_cancel = QPushButton("Cancel")
+        self.btn_TEST_PRINT = QPushButton("TESTING: Print samples")
 
         self.side_edit_btn_layout.addWidget(self.btn_edit)
         self.side_edit_btn_layout.addWidget(self.btn_custom)
         self.side_edit_btn_layout.addWidget(self.btn_save)
         self.side_edit_btn_layout.addWidget(self.btn_cancel)
+        self.side_edit_btn_layout.addWidget(self.btn_TEST_PRINT)
         self.side_edit_btn_layout.addStretch()
 
         self.selector = QListWidget()
 
+        self.PROPERTY_COL_INDEX = 0
+        self.VALUE_COL_INDEX = 1
         self.info_table = QTableWidget()
         self.info_table.setColumnCount(2)
-        self.info_table.setHorizontalHeaderItem(0, QTableWidgetItem("Sample property"))
-        self.info_table.setHorizontalHeaderItem(1, QTableWidgetItem("Value"))
+        property_item = QTableWidgetItem("Sample property")
+        value_item = QTableWidgetItem("Value")
+        self.info_table.setHorizontalHeaderItem(self.PROPERTY_COL_INDEX, property_item)
+        self.info_table.setHorizontalHeaderItem(self.VALUE_COL_INDEX, value_item)
+        self.info_table.setSelectionMode(QAbstractItemView.SingleSelection)
 
         self.horizontal_layout = QHBoxLayout()
         self.horizontal_layout.addWidget(self.selector)
@@ -53,6 +67,14 @@ class SamplePanelWidgets(QWidget):
         self.layout.addLayout(self.top_add_remove_btn_layout)
         self.layout.addLayout(self.horizontal_layout)
         self.sample_panel_widget.setLayout(self.layout)
+
+    def create_add_row_button(self, signal=None):
+        self.add_row_btn = QPushButton("Add row")
+        self.add_row_btn.clicked.connect(signal)
+
+    def create_delete_row_button(self, signal=None):
+        self.delete_row_btn = QPushButton("Delete row")
+        self.delete_row_btn.clicked.connect(signal)
 
 
 class AddSampleDialog(QDialog):
