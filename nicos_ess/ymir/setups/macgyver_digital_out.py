10c2d49398f14f432bf919e3d685e3da2bd62d1e
Matt Clarke <matt.clarke@ess.eu>
2025-01-15 13:56:19 +0100

    macgyver fixes

--- nicos_ess/ymir/setups/macgyver_digital_out.py
+++ nicos_ess/ymir/setups/macgyver_digital_out.py
@@ -6,7 +6,7 @@ devices = dict()
 
 for i in range(1, 9):
     devices[f"macgyver_digital_out_{i}"] = device(
-        "nicos_ess.devices.epics.pva.EpicsBoolMoveable",
+        "nicos_ess.devices.epics.pva.EpicsMappedMoveable",
         description=f"MacGyver box digital out {i}",
         readpv=f"{pv_root}digital_out_{i}-R",
         writepv=f"{pv_root}digital_out_{i}-S",
