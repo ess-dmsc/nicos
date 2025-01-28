8b6900c31212b7c202ea1a84cd3a2b15c21246bb
Matt Clarke <matt.clarke@ess.eu>
2025-01-28 09:46:36 +0000

    Update 2 files
    - /nicos/devices/epics/pva/epics_devices.py
    - /nicos_ess/devices/epics/pva/epics_devices.py

--- nicos/devices/epics/pva/epics_devices.py
+++ nicos/devices/epics/pva/epics_devices.py
@@ -501,7 +501,7 @@ class EpicsMappedReadable(MappedReadable, EpicsReadable):
         # MBBI, BI, etc. do not have units
         "unit": Override(mandatory=False, settable=False, volatile=False),
         # Mapping values are read from EPICS
-        "mapping": Override(internal=True, settable=False, userparam=False),
+        'mapping': Override(internal=True, mandatory=False, settable=False)
     }
 
     def _get_pv_parameters(self):
@@ -562,7 +562,7 @@ class EpicsMappedMoveable(MappedMoveable, EpicsMoveable):
         # MBBI, BI, etc. do not have units
         "unit": Override(mandatory=False, settable=False, volatile=False),
         # Mapping values are read from EPICS
-        "mapping": Override(internal=True, settable=False, userparam=False),
+        'mapping': Override(internal=True, mandatory=False, settable=False)
     }
 
     def _get_pv_parameters(self):
