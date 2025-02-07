d1748b8f2ca6da4a063dfc6e5ec5b7a1f3246dab
linemoller <line.moller@ess.eu>
2025-02-07 12:37:48 +0100

    edit samples header

--- nicos_ess/gui/widgets/sample_widgets.py
+++ nicos_ess/gui/widgets/sample_widgets.py
@@ -79,7 +79,7 @@ class SamplePanelWidgets(QWidget):
         self.btn_save_edit.setStyleSheet("background-color: limegreen")
         self.btn_save_prop.setStyleSheet("background-color: limegreen")
 
-        panel_header = QLabel("Sample information")
+        panel_header = QLabel("Samples")
         panel_header.setStyleSheet(
             "font-size: 20pt;" "font-weight: 50;" "padding-top: 20px;"
         )
