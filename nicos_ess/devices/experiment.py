dfa96791d332cc322a0de6afafbd7f175f648669
Lais Pessine do Carmo <lais.pessine@ess.eu>
2025-02-25 14:52:00 +0100

    More notifications clean up in nicos_ess/

--- nicos_ess/devices/experiment.py
+++ nicos_ess/devices/experiment.py
@@ -301,7 +301,6 @@ class EssExperiment(Device):
                 "localcontacts": [],
                 "samples": self._extract_samples(prop),
                 "dataemails": [],
-                "notif_emails": [],
                 "errors": [],
                 "warnings": [],
             }
