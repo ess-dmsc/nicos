f4ae99c1b073f99960528f069be0c116d59f2237
Jonas Petersson <jonas.petersson@ess.eu>
2025-02-28 13:34:02 +0100

    add to bifrost

---
+++ nicos_ess/bifrost/setups/beamlime.py
@@ -0,0 +1,31 @@
+description = "The beamlime."
+
+devices = dict(
+    detector_sliding=device(
+        "nicos_ess.devices.datasources.beamlime.DataChannel",
+        description="A just-bin-it image channel",
+        source_name="unified_detector:sliding",
+        type="counter",
+    ),
+    detector_cumulative=device(
+        "nicos_ess.devices.datasources.beamlime.DataChannel",
+        description="A just-bin-it image channel",
+        source_name="unified_detector:cumulative",
+        type="counter",
+    ),
+    detector_roi=device(
+        "nicos_ess.devices.datasources.beamlime.DataChannel",
+        description="A just-bin-it image channel",
+        source_name="unified_detector:roi",
+        type="counter",
+    ),
+    beamlime_collector=device(
+        "nicos_ess.devices.datasources.beamlime.BeamLimeCollector",
+        description="The just-bin-it histogrammer",
+        brokers=["10.100.4.15:8093", "10.100.4.17:8093", "10.100.5.29:8093"],
+        topic=["bifrost_beamlime_data"],
+        command_topic="bifrost_beamlime_data",
+        others=["detector_sliding", "detector_cumulative", "detector_roi"],
+        schema="da00",
+    ),
+)
