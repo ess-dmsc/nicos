4dcb47d800a36c2505f9b0b218047b0990363018
Jonas Petersson <jonas.petersson@ess.eu>
2025-02-28 13:43:45 +0100

    wrong source names

--- nicos_ess/bifrost/setups/beamlime.py
+++ nicos_ess/bifrost/setups/beamlime.py
@@ -4,19 +4,19 @@ devices = dict(
     detector_sliding=device(
         "nicos_ess.devices.datasources.beamlime.DataChannel",
         description="A just-bin-it image channel",
-        source_name="unified_detector:sliding",
+        source_name="unified_detector:unified_detector/sliding",
         type="counter",
     ),
     detector_cumulative=device(
         "nicos_ess.devices.datasources.beamlime.DataChannel",
         description="A just-bin-it image channel",
-        source_name="unified_detector:cumulative",
+        source_name="unified_detector:unified_detector/cumulative",
         type="counter",
     ),
     detector_roi=device(
         "nicos_ess.devices.datasources.beamlime.DataChannel",
         description="A just-bin-it image channel",
-        source_name="unified_detector:roi",
+        source_name="unified_detector:unified_detector/roi",
         type="counter",
     ),
     beamlime_collector=device(
