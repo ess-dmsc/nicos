c0804d5a294279b14db52655387cb842ac6527b8
Lais Pessine Do Carmo <lais.pessine@ess.eu>
2025-03-19 16:02:45 +0000

    NMX: Fix instrument name in its setup

--- nicos_ess/nmx/testscripts/basic.py
+++ nicos_ess/nmx/testscripts/basic.py
@@ -1,6 +1,6 @@
 # pylint: skip-file
 
-# test: subdirs = loki
+# test: subdirs = nmx
 # test: setups = aperture,beamstop,chopper_1,chopper_2,collimator_1,collimator_2,detectors,gate_valve,laser,monitors,shutters,slit_set_1,slit_set_2,slit_set_3
 # test: needs = streaming_data_types
 # test: needs = confluent_kafka
