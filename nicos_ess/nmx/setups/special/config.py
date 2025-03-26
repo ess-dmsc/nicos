7af54cb335dce7e3d53f27652cf16780d06296ec
George O'Neill <georgeoneill@bgtestinghost.daq.esss.dk>
2025-03-26 15:25:04 +0100

    kafka brokers

--- nicos_ess/nmx/setups/special/config.py
+++ nicos_ess/nmx/setups/special/config.py
@@ -3,7 +3,5 @@ description = "High-level configuration settings"
 group = "configdata"
 
 KAFKA_BROKERS = [
-    "10.100.4.13:8093",
-    "10.100.5.17:8093",
-    "10.100.6.19:8093",
+    "127.0.0.1:8093",
 ]
