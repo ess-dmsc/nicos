0f5ba6b57244a22dd2b0697fc2056579321df4f2
Jonas Petersson <jonas.petersson@ess.eu>
2025-04-01 09:55:18 +0200

    rookie mistake

--- nicos_ess/bifrost/setups/detector_power_supplies.py
+++ nicos_ess/bifrost/setups/detector_power_supplies.py
@@ -4,28 +4,28 @@ pv_root_1 = "BIFRO-Det:PwrC-HVPS-001:"
 
 devices = dict()
 
-for i in range(0, 3):
+for i in range(0, 4):
     devices[f"voltage_{i}"] = device(
         "nicos_ess.devices.epics.pva.EpicsReadable",
         description=f"Detector HVPS current {i}",
         readpv=f"{pv_root_1}Ch{i}-Current-R",
     )
 
-for i in range(0, 3):
+for i in range(0, 4):
     devices[f"current_{i}"] = device(
         "nicos_ess.devices.epics.pva.EpicsReadable",
         description=f"Detector HVPS voltage {i}",
         readpv=f"{pv_root_1}Ch{i}-Voltage-R",
     )
 
-for i in range(0, 3):
+for i in range(0, 4):
     devices[f"status_{i}"] = device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description=f"Detector HVPS status on {i}",
         readpv=f"{pv_root_1}Ch{i}-StatOn-R",
     )
 
-for i in range(0, 3):
+for i in range(0, 4):
     devices[f"status_{i}"] = device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description=f"Detector HVPS enable on {i}",
