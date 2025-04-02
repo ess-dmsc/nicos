d21d66dadae5d3a0891e686e6b0ee7747f086e11
linemoller <line.moller@ess.eu>
2025-04-02 15:21:37 +0200

    add pv for output source

--- nicos_ess/loki/setups/detector_ups.py
+++ nicos_ess/loki/setups/detector_ups.py
@@ -3,17 +3,16 @@ description = "The UPS for the LoKI detector"
 pv_root = "LOKI-Det:NDet-UPS-001:"
 
 devices = dict(
-    battery_status_int=device(
-        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
-        description="The status of the battery",
-        readpv="{}BatteryStatusInt-R".format(pv_root),
-        mapping={"Unknown": 1, "Normal": 2, "Low": 3, "Depleted": 4},
-        visibility=(),
-    ),
-    battery_status_str=device(
+    battery_status=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="The status of the battery",
         readpv="{}BatteryStatusStr-R".format(pv_root),
+        visibility=(),
+    ),
+    output_source=device(
+        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
+        description="Present source of output power in string. None mean no output at all.",
+        readpv="{}OutputSourceStr-R".format(pv_root),
         visibility=(),
     ),
 )
