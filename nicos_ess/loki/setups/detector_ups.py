80b5e4cbcf998d289724ff4f1cb5e71cd5d98a58
linemoller <line.moller@ess.eu>
2025-04-02 15:12:53 +0200

    add status string as well for testing

--- nicos_ess/loki/setups/detector_ups.py
+++ nicos_ess/loki/setups/detector_ups.py
@@ -3,11 +3,17 @@ description = "The UPS for the LoKI detector"
 pv_root = "LOKI-Det:NDet-UPS-001:"
 
 devices = dict(
-    battery_status=device(
+    battery_status_int=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="The status of the battery",
         readpv="{}BatteryStatusInt-R".format(pv_root),
         mapping={"Unknown": 1, "Normal": 2, "Low": 3, "Depleted": 4},
         visibility=(),
-    )
+    ),
+    battery_status_str=device(
+        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
+        description="The status of the battery",
+        readpv="{}BatteryStatusStr-R".format(pv_root),
+        visibility=(),
+    ),
 )
