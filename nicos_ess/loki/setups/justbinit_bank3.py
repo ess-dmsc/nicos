87e68171f039ba0627950e3cbb14aa59428a6fff
linemoller <line.moller@ess.eu>
2025-04-07 14:43:11 +0200

    breaking banks into separate files

---
+++ nicos_ess/loki/setups/justbinit_bank3.py
@@ -0,0 +1,44 @@
+description = "The just-bin-it histogrammer."
+
+devices = dict(
+    bank3_2d_det_image=device(
+        "nicos_ess.devices.datasources.just_bin_it.JustBinItImage",
+        description="A just-bin-it image channel",
+        hist_topic="loki_visualisation",
+        data_topic="loki_detector_bank3",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        unit="evts",
+        hist_type="2-D DET",
+        det_width=512,
+        det_height=448,
+        det_range=(1204225, 1433600),
+    ),
+    bank3_1d_tof_image=device(
+        "nicos_ess.devices.datasources.just_bin_it.JustBinItImage",
+        description="A just-bin-it image channel",
+        hist_topic="loki_visualisation",
+        data_topic="loki_detector_bank3",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        unit="evts",
+        hist_type="1-D TOF",
+        det_width=512,
+        det_height=448,
+        det_range=(1204225, 1433600),
+        tof_range=(0, 100000000),
+    ),
+    bank3_det=device(
+        "nicos_ess.devices.datasources.just_bin_it.JustBinItDetector",
+        description="The just-bin-it histogrammer",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        unit="",
+        command_topic="loki_jbi_commands",
+        response_topic="loki_jbi_responses",
+        statustopic=["loki_jbi_heartbeat"],
+        images=["bank3_2d_det_image", "bank3_1d_tof_image"],
+        hist_schema="hs01",
+    ),
+)
+
+startupcode = """
+SetDetectors(bank3_det)
+"""
---
+++ nicos_ess/loki/setups/justbinit_bank5.py
@@ -0,0 +1,44 @@
+description = "The just-bin-it histogrammer."
+
+devices = dict(
+    bank5_2d_det_image=device(
+        "nicos_ess.devices.datasources.just_bin_it.JustBinItImage",
+        description="A just-bin-it image channel",
+        hist_topic="loki_visualisation",
+        data_topic="loki_detector_bank5",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        unit="evts",
+        hist_type="2-D DET",
+        det_width=512,
+        det_height=784,
+        det_range=(1605633, 2007040),
+    ),
+    bank5_1d_tof_image=device(
+        "nicos_ess.devices.datasources.just_bin_it.JustBinItImage",
+        description="A just-bin-it image channel",
+        hist_topic="loki_visualisation",
+        data_topic="loki_detector_bank5",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        unit="evts",
+        hist_type="1-D TOF",
+        det_width=512,
+        det_height=784,
+        det_range=(1605633, 2007040),
+        tof_range=(0, 100000000),
+    ),
+    bank5_det=device(
+        "nicos_ess.devices.datasources.just_bin_it.JustBinItDetector",
+        description="The just-bin-it histogrammer",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        unit="",
+        command_topic="loki_jbi_commands",
+        response_topic="loki_jbi_responses",
+        statustopic=["loki_jbi_heartbeat"],
+        images=["bank5_2d_det_image", "bank5_1d_tof_image"],
+        hist_schema="hs01",
+    ),
+)
+
+startupcode = """
+SetDetectors(bank5_det)
+"""
