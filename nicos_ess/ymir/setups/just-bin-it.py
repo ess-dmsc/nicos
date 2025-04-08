89fe823e8bfde6c3a675acd6f35e8ee3d9041e13
Jonas Petersson <jonas.petersson@ess.eu>
2025-04-08 15:47:19 +0200

    add other channels to jbi

--- nicos_ess/ymir/setups/just-bin-it.py
+++ nicos_ess/ymir/setups/just-bin-it.py
@@ -26,6 +26,18 @@ devices = dict(
         det_range=(1, 12800),
         tof_range=(0, 1000000),
     ),
+    timer=device(
+        "nicos.devices.generic.VirtualTimer",
+        description="Simulated TIM1",
+        fmtstr="%.2f",
+        unit="s",
+    ),
+    pulse_counter=device(
+        "nicos_ess.devices.epics.pulse_counter.PulseCounter",
+        description="EVR Pulse Counter",
+        readpv="YMIR-TS:Ctrl-EVR-01:EvtACnt-I",
+        fmtstr="%d",
+    ),
     det=device(
         "nicos_ess.devices.datasources.just_bin_it.JustBinItDetector",
         description="The just-bin-it histogrammer",
@@ -35,6 +47,8 @@ devices = dict(
         response_topic="ymir_jbi_responses",
         statustopic=["ymir_jbi_heartbeat"],
         images=["det_image1", "det_image2"],
+        timers=["timer"],
+        counters=["pulse_counter"],
         hist_schema="hs01",
     ),
 )
