eae794169c334c151a24d9b93fbdffbfa4e5ea6f
linemoller <line.moller@ess.eu>
2025-04-30 13:22:21 +0200

    remove epicsshutter device since there will be no controlling with writepv

--- nicos_ess/loki/setups/heavy_shutter.py
+++ nicos_ess/loki/setups/heavy_shutter.py
@@ -2,18 +2,7 @@ description = "LoKI heavy shutter"
 
 pv_root = "LOKI-HvSht:MC-Pne-01:"
 
-### IN PROGRESS!!
-
 devices = dict(
-    heavy_shutter=device(
-        "nicos_ess.devices.epics.pva.shutter.EpicsShutter",
-        description="Heavy shutter - pneumatic axis 1 in motion cabinet 1",
-        readpv=f"{pv_root}ShtAuxBits07",
-        pva=True,
-        monitor=True,
-        pollinterval=0.5,
-        maxage=None,
-    ),
     heavy_shutter_status=device(
         "nicos_ess.devices.epics.pva.EpicsStringReadable",
         description="Status of the heavy shutter",
