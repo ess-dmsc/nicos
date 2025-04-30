6d2a49cabcda097a5935212b6654cf509b7bd4f0
linemoller <line.moller@ess.eu>
2025-04-30 14:08:35 +0200

    change to mappedreadables

--- nicos_ess/loki/setups/heavy_shutter.py
+++ nicos_ess/loki/setups/heavy_shutter.py
@@ -4,12 +4,12 @@ pv_root = "LOKI-HvSht:MC-Pne-01:"
 
 devices = dict(
     heavy_shutter=device(
-        "nicos_ess.devices.epics.pva.EpicsStringReadable",
+        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Heavy shutter - pneumatic axis 1 in motion cabinet 1",
         readpv=f"{pv_root}ShtMsgTxt",
     ),
     heavy_shutter_error=device(
-        "nicos_ess.devices.epics.pva.EpicsStringReadable",
+        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Error status of the heavy shutter",
         readpv=f"{pv_root}ShtErr",
     ),
