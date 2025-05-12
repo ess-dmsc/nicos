3cc42fb9dd970582a85a47e6aacbb60f045e0fd9
Jonas Petersson <jonas.petersson@ess.eu>
2025-05-12 11:15:13 +0200

    add the tbl beam monitor

---
+++ nicos_ess/ymir/setups/tbl_beamlime.py
@@ -0,0 +1,25 @@
+description = "The beamlime interface for tbl."
+
+devices = dict(
+    monitor1_current=device(
+        "nicos_ess.devices.datasources.beamlime.DataChannel",
+        description="A just-bin-it image channel",
+        source_name="monitor1/monitor_data/current",
+        type="counter",
+    ),
+    monitor1_cumulative=device(
+        "nicos_ess.devices.datasources.beamlime.DataChannel",
+        description="A just-bin-it image channel",
+        source_name="monitor1/monitor_data/cumulative",
+        type="counter",
+    ),
+    beamlime_collector=device(
+        "nicos_ess.devices.datasources.beamlime.BeamLimeCollector",
+        description="The beamlime detector collector",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        topic=["tbl_beamlime_data"],
+        command_topic="tbl_beamlime_commands",
+        others=["monitor1_current", "monitor1_cumulative"],
+        schema="da00",
+    ),
+)
