21ee9772c87aaa3796aa932e27755bb1f10550b7
Jonas Petersson <jonas.petersson@ess.eu>
2025-05-21 15:58:15 +0200

    change readbacks to be same as setpoints

--- nicos_ess/bifrost/setups/cryo_magnet.py
+++ nicos_ess/bifrost/setups/cryo_magnet.py
@@ -55,7 +55,7 @@ devices = dict(
     flush=device(
         "nicos_ess.devices.epics.manual_switch.ManualSwitch",
         description="Start/stop the Flush cycle",
-        readpv=f"{pv_root}Flush-status-r",
+        readpv=f"{pv_root}Flush-s",
         writepv=f"{pv_root}Flush-s",
         states=["False", "True"],
         mapping={"False": 0, "True": 1},
@@ -96,7 +96,7 @@ devices = dict(
     p_reg=device(
         "nicos_ess.devices.epics.manual_switch.ManualSwitch",
         description="Pressure-regulator mode",
-        readpv=f"{pv_root}PReg-status-r",
+        readpv=f"{pv_root}PReg-s",
         writepv=f"{pv_root}PReg-s",
         states=["position", "pressure", "table"],
         mapping={"position": 0, "pressure": 1, "table": 2},
@@ -109,7 +109,7 @@ devices = dict(
     ln2f_fill=device(
         "nicos_ess.devices.epics.manual_switch.ManualSwitch",
         description="Start/stop LN2 fill",
-        readpv=f"{pv_root}LN2F-status-r",
+        readpv=f"{pv_root}LLN2F-Fill-s",
         writepv=f"{pv_root}LN2F-Fill-s",
         states=["False", "True"],
         mapping={"False": 0, "True": 1},
@@ -117,7 +117,7 @@ devices = dict(
     ln2f_auto=device(
         "nicos_ess.devices.epics.manual_switch.ManualSwitch",
         description="LN2 auto-fill on/off",
-        readpv=f"{pv_root}LN2F-status-r",
+        readpv=f"{pv_root}LN2F-Auto-s",
         writepv=f"{pv_root}LN2F-Auto-s",
         states=["False", "True"],
         mapping={"False": 0, "True": 1},
