27abdc6b25b856174e2026a5db35185da8a168d9
Jonas Petersson <jonas.petersson@ess.eu>
2025-06-02 10:22:14 +0200

    add the beamlime config for tbl

---
+++ nicos_ess/tbl/setups/beamlime.py
@@ -0,0 +1,26 @@
+description = "The beamlime interface for tbl."
+
+devices = dict(
+    monitor1_current=device(
+        "nicos_ess.devices.datasources.beamlime.DataChannel",
+        description="Sliding time window monitor",
+        source_name="monitor1/monitor_data/current",
+        type="counter",
+    ),
+    monitor1_cumulative=device(
+        "nicos_ess.devices.datasources.beamlime.DataChannel",
+        description="Accumulated monitor",
+        source_name="monitor1/monitor_data/cumulative",
+        type="counter",
+    ),
+    beamlime_collector=device(
+        "nicos_ess.devices.datasources.beamlime.BeamLimeCollector",
+        description="The beamlime detector collector",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        topic=["tbl_beamlime_data"],
+        command_topic="tbl_beamlime_commands",
+        cfg_group_id="nicos-tbl-beamlime-cfg",
+        others=["monitor1_current", "monitor1_cumulative"],
+        schema="da00",
+    ),
+)
