60a8e209811e03efa9dfe7fd7653b306a5a30dba
George Kontogiorgos <george.kontogiorgos@ess.eu>
2025-06-04 13:45:44 +0000

    mapped_controller: implement class that allow multiple targets

--- nicos_ess/tbl/setups/motion_cabinet_2.py
+++ nicos_ess/tbl/setups/motion_cabinet_2.py
@@ -1,40 +1,6 @@
 description = "Motion cabinet 2"
 
 devices = dict(
-    attenuator_changer_axis=device(
-        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Attenuator changer",
-        motorpv="TBL-AttChg:MC-LinY-01:Mtr",
-        monitor_deadband=0.01,
-    ),
-    attenuator_changer_controller=device(
-        "nicos_ess.devices.mapped_controller.MappedController",
-        controlled_device="attenuator_changer_axis",
-        mapping={"attenuator_1": 0, "attenuator_2": 0, "attenuator_3": 20},
-    ),
-    collimator_horizontal_axis=device(
-        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimator horizontal",
-        motorpv="TBL-PinLin:MC-LinY-01:Mtr",
-        monitor_deadband=0.01,
-    ),
-    collimator_vertical_axis=device(
-        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimator horizontal",
-        motorpv="TBL-PinLif:MC-LinZ-01:Mtr",
-        monitor_deadband=0.01,
-    ),
-    pinhole_changer_axis=device(
-        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Pinhole changer",
-        motorpv="TBL-PinChg:MC-LinY-01:Mtr",
-        monitor_deadband=0.01,
-    ),
-    pinhole_changer_controller=device(
-        "nicos_ess.devices.mapped_controller.MappedController",
-        controlled_device="pinhole_changer_axis",
-        mapping={"pinhole_1": 0, "pinhole_2": 0, "pinhole_3": 20},
-    ),
     filter_changer_axis_1=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Filter changer 1",
