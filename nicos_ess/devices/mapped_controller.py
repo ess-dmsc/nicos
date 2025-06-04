6bc9ea7a6f00cd95a5f59f962b8d6508484cc070
George Nicolas Kontogiorgos <georgekontogiorgos@ess.eu>
2025-06-04 15:28:35 +0200

    mapped_controller: Implement composer

--- nicos_ess/devices/mapped_controller.py
+++ nicos_ess/devices/mapped_controller.py
@@ -6,6 +6,7 @@ from nicos.core import (
     InvalidValueError,
     oneof,
     Param,
+    anytype,
     tupleof,
     status,
 )
@@ -167,3 +168,87 @@ class MultiTargetMapping(MappedMoveable):
         if not mapped_value:
             return "In Between"
         return mapped_value
+
+class MultiTargetSelector(MappedController):
+    """
+    Selector amongst some keys and sends it to MultiTargetComposer
+    that can handle several different MultiTargetSelector as channels
+    """
+
+    parameters = {
+        "mapped_selected_value": Param(
+            "Selected value from map (MultiTargetSelector class internal use only)",
+            unit="main",
+            fmtstr="main",
+            type=anytype,
+            default=None,
+            settable=True
+        )
+    }
+
+    def doInit(self, mode):
+        MappedController.doInit(self, mode)
+
+    def doStart(self, value):
+        self.mapped_selected_value = self.mapping.get(value, None)
+        self._attached_controlled_device.doStart(value)
+
+    def doRead(self, maxage=0):
+        return self.mapped_selected_value
+
+    def doStatus(self, maxage=0):
+        return self._attached_controlled_device.doStatus(maxage)
+
+class MultiTargetComposer(Moveable):
+    """
+    Concatenates strings from a list of MultiTargetSelector
+    and sends it to MultiTargetMapping
+    """
+    
+    parameters = {
+        "composerstatus": Param(
+            "Current status that regards the mapping composer",
+            type=tupleof(int, str),
+            settable=True,
+            default=(status.OK, "idle"),
+            no_sim_restore=True,
+        ),
+    }
+
+    attached_devices = {
+        "composition_inputs": Attach("Selector channels", Moveable, multiple=True),
+        "composition_output": Attach("Output to MultiTargetMapping", Moveable),
+    }
+
+    def doInit(self, mode):
+        for input in self._attached_composition_inputs:
+            print(input)
+
+    def doStart(self, value):
+        full_key=""
+
+        for input in self._attached_composition_inputs:
+            individual_key=input.doRead()
+            if individual_key is None:
+                message = (
+                    f"Selector '{input}' is None, "\
+                    "perhaps you forgot setting them? "\
+                    "I will not move anything."
+                )
+                self.composerstatus = (status.WARN, message)
+                break
+            else:
+                self.composerstatus = (status.OK, 'idle')
+                full_key+=str(individual_key)
+
+        else:
+            self._attached_composition_output.doStart(full_key)
+
+    def doRead(self, maxage=0):
+        return self._attached_composition_output.doRead(maxage)
+
+    def doStatus(self, maxage=0):
+        return max(
+            self._attached_composition_output.doStatus(maxage),
+            self.composerstatus
+        )
