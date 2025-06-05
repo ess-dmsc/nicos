6e28d903e9bd54f3847ddd394939559934fa91eb
Jonas Petersson <jonas.petersson@ess.eu>
2025-06-05 12:47:22 +0000

    Tbl powerauto and add monitor info

--- nicos_ess/tbl/setups/motion_cabinet_1.py
+++ nicos_ess/tbl/setups/motion_cabinet_1.py
@@ -2,17 +2,29 @@ description = "Motion cabinet 1"
 
 devices = dict(
     heavy_shutter=device(
-        "nicos_ess.devices.epics.pva.shutter.EpicsShutter",
+        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Heavy Shutter",
         readpv="TBL-HvSht:MC-Pne-01:ShtAuxBits07",
-        pva=True,
-        monitor=True,
-        pollinterval=0.5,
-        maxage=None,
     ),
     heavy_shutter_status=device(
         "nicos_ess.devices.epics.pva.EpicsStringReadable",
         description="Status of the heavy shutter",
         readpv="TBL-HvSht:MC-Pne-01:ShtMsgTxt",
+    ),
+    cabinet_1_status=device(
+        "nicos_ess.devices.epics.mbbi_direct.MBBIDirectStatus",
+        description="Cabinet 1 status",
+        pv_root="TBL-MCS1:MC-MCU-01:Cabinet",
+        number_of_bits=24,
+    ),
+    cabinet_1_pressure_1=device(
+        "nicos_ess.devices.epics.pva.EpicsReadable",
+        description="Cabinet 1 pressure 1",
+        readpv="TBL-MCS1:MC-MCU-01:Pressure1",
+    ),
+    cabinet_1_pressure_2=device(
+        "nicos_ess.devices.epics.pva.EpicsReadable",
+        description="Cabinet 1 pressure 2",
+        readpv="TBL-MCS1:MC-MCU-01:Pressure2",
     ),
 )
