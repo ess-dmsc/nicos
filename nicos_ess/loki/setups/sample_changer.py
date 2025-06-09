8fcdd6f0cb0e3667b53f4de6db02f2e305935a8e
linemoller <line.moller@ess.eu>
2025-06-09 13:29:52 +0200

    drafts of setup files

---
+++ nicos_ess/loki/setups/sample_changer.py
@@ -0,0 +1,19 @@
+description = "LoKI sample changer"
+
+# devices = dict(
+#     linear_sample_changer=device(
+#         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+#         description="Linear Rotating Sample Cells Changer - electrical axis 9 in motion cabinet 3",
+#         motorpv="SpChg1:MC-LinY-01",
+#     ),
+#     rotation_sample_changer=device(
+#         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+#         description="Linear rotating sample cells changer - electrical axis 11 in motion cabinet 3",
+#         motorpv="SpCel1:MC-RotX-01",
+#     ),
+#     rotation_stage=device(
+#         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+#         description="Sample stack rotation stage - electrical axis 8 in motion cabinet 3",
+#         motorpv="SpRot1:MC-RotZ-01",
+#     ),
+# )
---
+++ nicos_ess/loki/setups/sample_rotation.py
@@ -0,0 +1,19 @@
+description = "LoKI sample rotation system"
+#
+# devices = dict(
+#     goniometer_x=device(
+#         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+#         description="Sample goniometer 1 - electrical axis 6 in motion cabinet 3",
+#         motorpv="SpGon1:MC-RotX-01",
+#     ),
+#     goniometer_y=device(
+#         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+#         description="Sample goniometer 2 - electrical axis 7 in motion cabinet 3",
+#         motorpv="SpGon2:MC-RotY-01",
+#     ),
+#     rotation_stage=device(
+#         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+#         description="Sample stack rotation stage - electrical axis 8 in motion cabinet 3",
+#         motorpv="SpRot1:MC-RotZ-01",
+#     ),
+# )
