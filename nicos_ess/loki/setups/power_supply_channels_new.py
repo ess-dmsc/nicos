28df4dfcaf6acf8c875ffc5003d090d4b8a7f325
Lais Pessine do Carmo <lais.pessine@ess.eu>
2025-06-13 16:42:06 +0200

    LOKI: Break line in PS config file

--- nicos_ess/loki/setups/power_supply_channels_new.py
+++ nicos_ess/loki/setups/power_supply_channels_new.py
@@ -54,7 +54,9 @@ devices = dict()
 for key, channel in all_channels.items():
     pv_root = channel["pv_root_channel"]
     channel_voltage = device(
-        "nicos_ess.devices.epics.pva.EpicsReadable", readpv=f"{pv_root}-VMon", unit="V"
+        "nicos_ess.devices.epics.pva.EpicsReadable", 
+        readpv=f"{pv_root}-VMon",
+        unit="V"
     )
     channel_current = device(
         "nicos_ess.devices.epics.pva.EpicsReadable",
