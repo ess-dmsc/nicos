1f8fc26a32b5008f8ed2818f1f7e4aed588cdba4
Renee Helfert <renee.helfert@ess.eu>
2025-06-25 09:49:01 +0200

    inital files for freia in NICOS

---
+++ nicos_ess/freia/setups/special/daemon.py
@@ -0,0 +1,29 @@
+description = "setup for the execution daemon"
+group = "special"
+
+devices = dict(
+    Auth=device(
+        "nicos.services.daemon.auth.list.Authenticator",
+        hashing="md5",
+        passwd=[
+            ("guest", "", "guest"),
+            ("user", "ee11cbb19052e40b07aac0ca060c23ee", "user"),
+            ("admin", "21232f297a57a5a743894a0e4a801fc3", "admin"),
+        ],
+    ),
+    LDAPAuth=device(
+        "nicos_ess.devices.auth.ldap.Authenticator",
+        uri=["dc01.esss.lu.se", "dc02.esss.lu.se", "dc03.esss.lu.se"],
+        userbasedn="dc=esss,dc=lu,dc=se",
+        grouproles={"ECDC": "user", "ECDC SE": "user"},
+    ),
+    Daemon=device(
+        "nicos.services.daemon.NicosDaemon",
+        server="",
+        authenticators=["Auth", "LDAPAuth"],
+        loglevel="debug",
+    ),
+)
+
+startupcode = """
+"""
