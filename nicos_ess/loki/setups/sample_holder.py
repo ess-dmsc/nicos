2b186fa8320911e0a7d5b0786c248535c6906765
Line Møller <line.moller@ess.eu>
2025-07-15 14:23:35 +0200

    loki sample holder update axes names

--- nicos_ess/loki/setups/sample_holder.py
+++ nicos_ess/loki/setups/sample_holder.py
@@ -3,19 +3,19 @@ description = "LoKI sample holder"
 group = "optional"
 
 devices = dict(
-    axis2_sample_stack_linear_x=device(
+    sample_stack_x=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Sample stack linear x - electrical axis 2 in motion cabinet 4",
         motorpv=f"LOKI-SpSt1:MC-LinX-01:Mtr",
         monitor_deadband=0.01,
     ),
-    axis3_sample_stack_linear_y=device(
+    sample_stack_y=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Sample stack linear y - electrical axis 3 in motion cabinet 4",
         motorpv=f"LOKI-SpSt1:MC-LinY-01:Mtr",
         monitor_deadband=0.01,
     ),
-    axis1_sample_stack_linear_z=device(
+    sample_stack_z=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Sample stack linear z - electrical axis 1 in motion cabinet 4",
         motorpv=f"LOKI-SpSt1:MC-LinZ-01:Mtr",
@@ -24,9 +24,9 @@ devices = dict(
     sample_stack_positioner=device(
         "nicos_ess.devices.mapped_controller.MultiTargetMapping",
         controlled_devices=[
-            "axis2_sample_stack_linear_x",
-            "axis3_sample_stack_linear_y",
-            "axis1_sample_stack_linear_z",
+            "sample_stack_x",
+            "sample_stack_y",
+            "sample_stack_z",
         ],
         mapping={
             "default_loading_position": (2, 198, 53.75),
@@ -35,13 +35,13 @@ devices = dict(
             "default_sample_holder_rotation": (120, 100, 0),
         },
     ),
-    axis9_linear_sample_changer=device(
+    linear_sample_changer=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Linear sample changer - electrical axis 9 in motion cabinet 3",
         motorpv="LOKI-SpChg1:MC-LinY-01:Mtr",
         monitor_deadband=0.01,
     ),
-    axis11_mixer_sample_rotation_cell=device(
+    sample_rotation=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Rotation sample cell - electrical axis 11 in motion cabinet 3",
         motorpv="LOKI-SpCel1:MC-RotX-01:Mtr",
@@ -50,8 +50,8 @@ devices = dict(
     thermostated_sample_holder=device(
         "nicos_ess.loki.devices.thermostated_cellholder.ThermoStatedCellHolder",
         description="The thermostated sample-holder for LoKI",
-        xmotor="axis9_linear_sample_changer",
-        ymotor="axis1_sample_stack_linear_z",
+        xmotor="linear_sample_changer",
+        ymotor="sample_stack_z",
         precision=[0.05, 0.05],
     ),
 )
