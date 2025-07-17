61fca907a4c351aa9ab34cbc9e7e8ba43366cb57
George Nicolas Kontogiorgos <georgekontogiorgos@ess.eu>
2025-07-17 12:36:25 +0200

    estia: fix mover PVs

--- nicos_ess/estia/setups/mover.py
+++ nicos_ess/estia/setups/mover.py
@@ -1,36 +1,34 @@
 description = "Selene1 mover motors"
 
-pvprefix = "PSI-ESTIARND:MC-MCU-01:"
-
 devices = dict(
     mover_fl_re_us=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="M1 Selene1 1-Mover FL-RE-US",
-        motorpv=f"{pvprefix}m1",
+        motorpv=f"ESTIA-SG1SM:MC-RotX-01:Mtr",
         has_powerauto=False,
     ),
     mover_pr_re_ds=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="M2 Selene1 1-Mover PR-RE-DS",
-        motorpv=f"{pvprefix}m2",
+        motorpv=f"ESTIA-SG1SM:MC-RotX-02:Mtr",
         has_powerauto=False,
     ),
     mover_pr_li_ds=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="M3 Selene1 1-Mover PR-LI-DS",
-        motorpv=f"{pvprefix}m3",
+        motorpv=f"ESTIA-SG1SM:MC-RotX-03:Mtr",
         has_powerauto=False,
     ),
     mover_pr_li_us1=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="M4 Selene1 2-Mover PR-LI-US-1",
-        motorpv=f"{pvprefix}m4",
+        motorpv=f"ESTIA-SG1DM:MC-RotX-01:",
         has_powerauto=False,
     ),
     mover_pr_li_us2=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="M5 Selene1 2-Mover PR-LI-US-2",
-        motorpv=f"{pvprefix}m5",
+        motorpv=f"ESTIA-SG1DM:MC-RotX-02:Mtr",
         has_powerauto=False,
     ),
 )
