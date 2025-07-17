b77a891dc372b4285ee1a85cf0cfe16235f01e48
linemoller <line.moller@ess.eu>
2025-07-17 14:32:17 +0200

    change pv and increase number of digits on value

--- nicos_ess/loki/setups/vacuum.py
+++ nicos_ess/loki/setups/vacuum.py
@@ -1,13 +1,14 @@
 description = "LOKI vacuum readouts (gauges and valves)"
 
 devices = dict(
-    vacuum_gauge=device(
+    sample_area_vacuum_gauge=device(
         "nicos_ess.devices.epics.pva.EpicsReadable",
         description="Vacuum gauge - MicroPirani sensor pressure (PR1), MKS910 IOC",
-        readpv="LOKI-VacInstr:Vac-VGF-050:PIRPrs-R",
+        readpv="LOKI-VacInstr:Vac-VGF-050:PR3-R",
+        fmtstr="%.6f",
         visiblitity=(),
     ),
-    vacuum_valve=device(
+    sample_area_vacuum_valve=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Vacuum valve - PLC",
         readpv="LOKI-VacInstr:Vac-VVS-400:OpenR",
