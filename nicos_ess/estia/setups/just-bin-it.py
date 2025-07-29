2192cf3a97e51429919b323967f6ec735093b5cf
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2025-07-29 15:06:57 +0200

    ESTIA: Update just-bin-it parameters

--- nicos_ess/estia/setups/just-bin-it.py
+++ nicos_ess/estia/setups/just-bin-it.py
@@ -2,10 +2,10 @@
 description = "JustBinIt histogrammer."
 
 devices = dict(
-    det_image1=device(
+    det_image=device(
         "nicos_ess.devices.datasources.just_bin_it.JustBinItImage",
         description="A just-bin-it image channel",
-        hist_topic="hist_topic_1",
+        hist_topic="estia_visualisation",
         data_topic="estia_detector",
         brokers=configdata("config.KAFKA_BROKERS"),
         source="just-bin-it",
@@ -13,26 +13,16 @@ devices = dict(
         hist_type="2-D TOF",
         det_range=(0, 10000),
     ),
-    det_image2=device(
-        "nicos_ess.devices.datasources.just_bin_it.JustBinItImage",
-        description="A just-bin-it image channel",
-        hist_topic="hist_topic_2",
-        data_topic="estia_detector",
-        brokers=configdata("config.KAFKA_BROKERS"),
-        source="just-bin-it",
-        unit="evts",
-        hist_type="1-D TOF",
-        det_range=(0, 10000),
-    ),
     det=device(
         "nicos_ess.devices.datasources.just_bin_it.JustBinItDetector",
         description="Just Bin it histogrammer",
         brokers=configdata("config.KAFKA_BROKERS"),
         unit="",
+        event_schema="ev44",
         command_topic="estia_jbi_commands",
         response_topic="estia_jbi_responses",
         statustopic=["estia_jbi_heartbeat"],
-        images=["det_image1", "det_image2"],
+        images=["det_image"],
         hist_schema="hs01",
         timers=["timer"],
     ),
