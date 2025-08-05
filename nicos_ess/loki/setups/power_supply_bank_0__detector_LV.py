433462ff1cc72a9e0b65c7f816edc7cfe1a8c09c
Lais Pessine do Carmo <lais.pessine@ess.eu>
2025-08-05 13:24:54 +0200

    LOKI: Update/Clean up PS bank setups
    - Using the new classes
    - Clean up unused params.
    - Remove PS channel devices from device dict (to hide duplicated info).

--- nicos_ess/loki/setups/power_supply_bank_0__detector_LV.py
+++ nicos_ess/loki/setups/power_supply_bank_0__detector_LV.py
@@ -8,6 +8,7 @@ description = "Power Supplies Bank 0 for the detector carriage (LV)."
 # Name of PS Bank and the list of channels selected for it.
 BANK_NAME = "LV_Bank_0"
 BANK_CHANNELS = [
+    # Each item in the list is a set of channels.
     {"ps_type": "LV", "board": "106", "channels": [f"{ch:>02}" for ch in range(0, 8)]},
     {"ps_type": "LV", "board": "107", "channels": [f"{ch:>02}" for ch in range(0, 6)]},
 ]
@@ -20,55 +21,26 @@ devices = dict()
 ps_channels = []
 
 for key in keys:
-    # NOTE: For now, devices must be created in the same file.
-    # Moving it to a reusable method power_supply_config.py lead to import errors.
-    # TODO: Simplify PSChhanel with PV subscription!
     channel = ALL_CHANNELS[key]
     pv_root = channel["pv_root_channel"]
-    channel_voltage = device(
-        "nicos_ess.devices.epics.pva.EpicsReadable",
-        readpv=f"{pv_root}-VMon",
-        unit="V"
-    )
-    channel_current = device(
-        "nicos_ess.devices.epics.pva.EpicsReadable",
-        readpv=f"{pv_root}-IMon",
-    )
-    channel_status = device(
-        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
-        readpv=f"{pv_root}-Status-ON",
-        mapping={"Channel is OFF": 0, "Channel is ON": 1},
-    )
-    channel_power_control = device(
-        "nicos_ess.devices.epics.pva.EpicsMappedMoveable",
-        readpv=f"{pv_root}-Pw-RB",
-        writepv=f"{pv_root}-Pw",
-        mapping={"OFF": 0, "ON": 1},
-    )
     power_supply_channel = device(
         "nicos_ess.devices.epics.power_supply_channel.PowerSupplyChannel",
         description=channel["description"],
+        board=0,  # TODO: Set board and channel properly.
+        channel=0,
         pollinterval=0.5,
         maxage=None,
-        unit="V",
-        fmtstr="%.3f",
-        voltage=channel_voltage,
-        current=channel_current,
-        status=channel_status,
-        power_control=channel_power_control,
+        ps_pv=pv_root,
         mapping={"OFF": 0, "ON": 1},
-        visibility={}
     )
-    devices[f"{key}_ps_channel"] = power_supply_channel
     ps_channels.append(power_supply_channel)
 
-# HV device
+# Bank device
 power_supply_module = device(
         "nicos_ess.devices.epics.power_supply_channel.PowerSupplyBank",
         description="Bank 0 LV Power Supplies (Detector Carriage)",
         pollinterval=1.0,
         maxage=None,
-        fmtstr="%.3f",
         ps_channels=ps_channels,
         mapping={"OFF": 0, "ON": 1},
     )
