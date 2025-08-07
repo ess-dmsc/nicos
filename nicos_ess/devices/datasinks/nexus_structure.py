ffab47dd25b6b676ec9d1b2e8220ef4f7d6655ec
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2025-08-07 16:23:54 +0200

    Remove samples_dict from try except testing block

--- nicos_ess/devices/datasinks/nexus_structure.py
+++ nicos_ess/devices/datasinks/nexus_structure.py
@@ -244,11 +244,7 @@ class NexusStructureJsonFile(NexusStructureProvider):
         if not samples_info:
             return structure
 
-        try:
-            samples_dict = samples_info[0][0]
-        except KeyError:
-            return structure
-
+        samples_dict = samples_info[0][0]
         samples_list = self._generate_samples_group_list(
             samples_dict, skip_keys=["number_of"]
         )
