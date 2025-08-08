550cc950b03e4d5d2ed681572fbccea788bb1dc0
Lais Pessine do Carmo <lais.pessine@ess.eu>
2025-08-08 15:17:55 +0200

    LOKI: Check if PSBank voltages are zero before moving detector

--- nicos_ess/loki/devices/detector_motion.py
+++ nicos_ess/loki/devices/detector_motion.py
@@ -20,6 +20,19 @@ class LOKIDetectorMotion(EpicsMotor):
         ),
     }
     
+    def bank_voltage_is_zero(self, ps_bank):
+
+        if ps_bank is None:
+            print("Detector motion: Power supply bank provided for the voltage check is None.")
+            return False # Return false to make check fail
+
+        for channel in ps_bank._attached_ps_channels:
+            if channel.doReadVoltage_Monitor() > 0:
+                print("Detector motion: Some power supply voltages are not zero.")
+                return False
+        print("Detector motion: Power supply voltages are zero.")
+        return True
+
     def doIsAllowed(self, pos):
         """ Hook method from the Device class to check if movement is allowed,
         by verifying if Power Supply Bank is OFF.
@@ -42,7 +55,15 @@ class LOKIDetectorMotion(EpicsMotor):
         except Exception as e:
             return False, "No Power Supply Bank found in setup ({}).".format(e)
 
-        _, bank_status_msg = ps_bank.status()
-        if "ON" in bank_status_msg:
-            return False, "Power Supply Bank is still ON (it should be OFF)."
+        bank_stat, _ = ps_bank.status()
+        bank_on, _ = ps_bank.status_on()
+        
+        if bank_stat != status.OK:
+            return False, "Power Supply Bank is in a NOT OK state."
+        if bank_on:
+            return False, "Power Supply Bank is still ON (its channels should be OFF)."
+        if not self.bank_voltage_is_zero(ps_bank):
+            return False, "Power Supply Bank is still ON (its voltages should be zero)."
+
+        print("Detector motion: Power Supply Bank is OFF. Moving is okay.")
         return True, "Power Supply Bank is OFF. Moving is okay."
