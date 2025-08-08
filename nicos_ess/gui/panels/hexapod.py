a5ab53d78f54e93d9640a967431b23b995fb8cad
Renee Helfert <renee.helfert@ess.eu>
2025-08-08 13:02:29 +0200

    figuring out some device param stuff

--- nicos_ess/gui/panels/hexapod.py
+++ nicos_ess/gui/panels/hexapod.py
@@ -18,6 +18,11 @@ from nicos_ess.gui.panels.panel import Panel
 from nicos_ess.gui.utils import get_icon
 
 
+class HexapodInfo(AttrDict):
+    def __init__():
+        return
+
+
 class HexapodPanel(Panel):
     """Provides a panel to view hexapod status. Currently a conceptual panel that
     will not properly connect to a hexapod. Emulates hexapod using virtual motors for each
@@ -31,10 +36,31 @@ class HexapodPanel(Panel):
 
         self.client = client
         self.device_panel = parent
-        # self.setup_connections(client)
+        self.options = options
+        self.grpSpd.setVisible(False)  # To Add
 
-    def setup_connections(self, client):
-        # client.cache.connect(self.on_client_cache)
-        client.connected.connect(self.on_client_connected)
-        client.setup.connect(self.on_client_setup)
-        client.disconnected.connect(self.on_client_disconnect)
+    def on_butStart_pressed(self):
+        # get data from spin boxes and add to the move command
+        self._get_hexapod_info()
+
+    def on_butStop_pressed(self):
+        self.exec_command("stop(hexapod)")
+
+    def exec_command(self, command):
+        self.client.tell("exec", command)
+
+    def _get_hexapod_info(self):
+        """Successful test to obtain the attached device vars with the names provided in setup.
+        Does not contain the parameters from hexapod, only attached devices
+        Next: Combine parameter data from hexapod and the attached devices with their own parameter data into
+        accessible dictionary data structure for manipulation"""
+
+        # This needs to be cleaned up
+        # Finds and sorts out the attached devices to the hexapod device
+        test = self.client.eval("session.getSetupInfo()", {})
+        self.hexapod_device = test["virtual_hexapod"]["devices"]["hexapod"][0]
+        self._adevs = test["virtual_hexapod"]["devices"]["hexapod"][1]
+        del self._adevs["description"]
+        length = len(self._adevs)
+
+        self.showError(f"{self.hexapod}")
