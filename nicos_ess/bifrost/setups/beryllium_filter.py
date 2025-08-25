974ad38b6f6bfb1baf3e18fab2389813e414a6e5
Jonas Petersson <jonas.petersson@ess.eu>
2025-08-25 16:47:54 +0200

    add the beryllium average temp

--- nicos_ess/bifrost/setups/beryllium_filter.py
+++ nicos_ess/bifrost/setups/beryllium_filter.py
@@ -1,12 +1,22 @@
 description = "Monitoring for the Beryllium filter"
 
-pv_prefix = "BIFRO-Det:Proc-TIC-001:PT100_CH"
+pv_prefix = "BIFRO-Det:Proc-TIC-001:"
+pv_channel_prefix = f"{pv_prefix}PT100_CH"
 
-devices = {}
+devices = {
+    "average_beryllium_filter_temperature": device(
+        "nicos_ess.devices.epics.pva.EpicsReadable",
+        description="Average temperature of channel 1, 3, 4, 8 and 9. Used to monitor filter temperature alarm.",
+        readpv=f"{pv_prefix}AvgFilterTemp",
+    ),
+}
 
 for i in range(1, 15):
+    desc = f"Temperature of PT100 channel {i}."
+    if i in [10, 13]:
+        desc += " Alarm state when filling is needed."
     devices[f"beryllium_filter_T{i:02d}"] = device(
         "nicos_ess.devices.epics.pva.EpicsReadable",
-        description=f"Temperature of PT100 channel {i}",
-        readpv=f"{pv_prefix}{i:02d}",
+        description=desc,
+        readpv=f"{pv_channel_prefix}{i:02d}",
     )
