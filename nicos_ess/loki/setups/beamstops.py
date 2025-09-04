d3bc1b4950225207afbad26448df5cc4bcf3b3da
linemoller <line.moller@ess.eu>
2025-09-04 14:44:46 +0200

    use loki beamstop controller and change order of controlled devices, also use absolute positions for mapped positions instead of offset

--- nicos_ess/loki/setups/beamstops.py
+++ nicos_ess/loki/setups/beamstops.py
@@ -21,7 +21,7 @@ devices = dict(
             "x position bs5": 5,  # TBD
         },
     ),
-    beamstop_y=device(  # offset = -32.5
+    beamstop_y=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Beamstop motor horizontal axis - electrical axis 4 in motion cabinet 5",
         motorpv=f"{pv_root}MC-LinY-01:Mtr",
@@ -30,9 +30,9 @@ devices = dict(
     beamstop_y_positioner=device(
         "nicos_ess.devices.mapped_controller.MappedController",
         controlled_device="beamstop_y",
-        mapping={"In beam": 0},
+        mapping={"In beam": 32.5},
     ),
-    beamstop1=device(  # offset = -54.630
+    beamstop1=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Detector beamstop Z1 M4 Transmission - electrical axis 5 in motion cabinet 5",
         motorpv=f"{pv_root}MC-LinZ-01:Mtr",
@@ -41,9 +41,9 @@ devices = dict(
     beamstop1_positioner=device(
         "nicos_ess.devices.mapped_controller.MappedController",
         controlled_device="beamstop1",
-        mapping={"Park": 932.87, "In beam": 0},  # park position = 987.5 + offset
+        mapping={"Park": 987.5, "In beam": 54.630},
     ),
-    beamstop2=device(  # offset = -59.320
+    beamstop2=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Detector beamstop Z2 20x25 - electrical axis 6 in motion cabinet 5",
         motorpv=f"{pv_root}MC-LinZ-02:Mtr",
@@ -52,9 +52,9 @@ devices = dict(
     beamstop2_positioner=device(
         "nicos_ess.devices.mapped_controller.MappedController",
         controlled_device="beamstop2",
-        mapping={"Park": 918.678, "In beam": 0},  # park pos = 977.998 + offset
+        mapping={"Park": 977.998, "In beam": 59.320},
     ),
-    beamstop3=device(  # offset = -55.600
+    beamstop3=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Detector beamstop Z3 50x60 - electrical axis 7 in motion cabinet 5",
         motorpv=f"{pv_root}MC-LinZ-03:Mtr",
@@ -63,9 +63,9 @@ devices = dict(
     beamstop3_positioner=device(
         "nicos_ess.devices.mapped_controller.MappedController",
         controlled_device="beamstop3",
-        mapping={"Park": 931.9, "In beam": 0},  # park position = 987.5 + offset
+        mapping={"Park": 987.5, "In beam": 55.6},
     ),
-    beamstop4=device(  # offset = -57.300
+    beamstop4=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Detector beamstop Z4 65x75 - electrical axis 8 in motion cabinet 5",
         motorpv=f"{pv_root}MC-LinZ-04:Mtr",
@@ -74,9 +74,9 @@ devices = dict(
     beamstop4_positioner=device(
         "nicos_ess.devices.mapped_controller.MappedController",
         controlled_device="beamstop4",
-        mapping={"Park": 929.7, "In beam": 0},  # park position = 987 + offset
+        mapping={"Park": 987, "In beam": 57.3},
     ),
-    beamstop5=device(  # offset = -54.600
+    beamstop5=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Detector beamstop Z5 100x105 - electrical axis 9 in motion cabinet 5",
         motorpv=f"{pv_root}MC-LinZ-05:Mtr",
@@ -85,72 +85,108 @@ devices = dict(
     beamstop5_positioner=device(
         "nicos_ess.devices.mapped_controller.MappedController",
         controlled_device="beamstop5",
-        mapping={"Park": 935.29, "In beam": 0},  # park position = 989.891 + offset
+        mapping={"Park": 989.891, "In beam": 54.6},
     ),
     beamstop_selector=device(
-        "nicos_ess.devices.mapped_controller.MultiTargetMapping",
+        "nicos_ess.loki.devices.loki_beamstop_controller.LokiBeamstopController",
         controlled_devices=[
+            "beamstop5_positioner",
+            "beamstop4_positioner",
+            "beamstop3_positioner",
+            "beamstop2_positioner",
+            "beamstop1_positioner",
             "beamstop_x_positioner",
             "beamstop_y_positioner",
-            "beamstop1_positioner",
-            "beamstop2_positioner",
-            "beamstop3_positioner",
-            "beamstop4_positioner",
-            "beamstop5_positioner",
         ],
         mapping={
             "Park all beamstops": (
                 "Park",
-                "In beam",
                 "Park",
                 "Park",
-                "Park",
-                "Park",
-                "Park",
-            ),
-            "Beamstop 1": (
-                "x position bs1",
-                "In beam",
-                "In beam",
-                "Park",
-                "Park",
-                "Park",
-                "Park",
-            ),
-            "Beamstop 2": (
-                "x position bs2",
-                "In beam",
-                "Park",
-                "In beam",
-                "Park",
-                "Park",
-                "Park",
-            ),
-            "Beamstop 3": (
-                "x position bs3",
-                "In beam",
-                "Park",
-                "Park",
-                "In beam",
-                "Park",
-                "Park",
-            ),
-            "Beamstop 4": (
-                "x position bs4",
-                "In beam",
                 "Park",
                 "Park",
                 "Park",
                 "In beam",
-                "Park",
             ),
             "Beamstop 5": (
-                "x position bs5",
                 "In beam",
                 "Park",
                 "Park",
                 "Park",
                 "Park",
+                "x position bs5",
+                "In beam",
+            ),
+            "Beamstop 5 + monitor": (
+                "In beam",
+                "Park",
+                "Park",
+                "Park",
+                "In beam",
+                "x position bs5",
+                "In beam",
+            ),
+            "Beamstop 4": (
+                "Park",
+                "In beam",
+                "Park",
+                "Park",
+                "Park",
+                "x position bs4",
+                "In beam",
+            ),
+            "Beamstop 4 + monitor": (
+                "Park",
+                "In beam",
+                "Park",
+                "Park",
+                "In beam",
+                "x position bs4",
+                "In beam",
+            ),
+            "Beamstop 3": (
+                "Park",
+                "Park",
+                "In beam",
+                "Park",
+                "Park",
+                "x position bs3",
+                "In beam",
+            ),
+            "Beamstop 3 + monitor": (
+                "Park",
+                "Park",
+                "In beam",
+                "Park",
+                "In beam",
+                "x position bs3",
+                "In beam",
+            ),
+            "Beamstop 2": (
+                "Park",
+                "Park",
+                "Park",
+                "In beam",
+                "Park",
+                "x position bs2",
+                "In beam",
+            ),
+            "Beamstop 2 + monitor": (
+                "Park",
+                "Park",
+                "Park",
+                "In beam",
+                "In beam",
+                "x position bs2",
+                "In beam",
+            ),
+            "Beamstop 1": (
+                "Park",
+                "Park",
+                "Park",
+                "Park",
+                "In beam",
+                "x position bs1",
                 "In beam",
             ),
         },
