fb312bfc2b7771c015e3e314f13e12ae6fd43ff8
linemoller <line.moller@ess.eu>
2025-09-05 11:02:40 +0200

    add admin level to move of the stack

--- nicos_ess/loki/setups/sample_stack.py
+++ nicos_ess/loki/setups/sample_stack.py
@@ -8,18 +8,21 @@ devices = dict(
         description="Sample stack linear x - electrical axis 2 in motion cabinet 4",
         motorpv=f"LOKI-SpSt1:MC-LinX-01:Mtr",
         monitor_deadband=0.01,
+        requires={"level": "admin"},
     ),
     sample_stack_y=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Sample stack linear y - electrical axis 3 in motion cabinet 4",
         motorpv=f"LOKI-SpSt1:MC-LinY-01:Mtr",
         monitor_deadband=0.01,
+        requires={"level": "admin"},
     ),
     sample_stack_z=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Sample stack linear z - electrical axis 1 in motion cabinet 4",
         motorpv=f"LOKI-SpSt1:MC-LinZ-01:Mtr",
         monitor_deadband=0.01,
+        requires={"level": "admin"},
     ),
     sample_stack_positioner=device(
         "nicos_ess.devices.mapped_controller.MultiTargetMapping",
