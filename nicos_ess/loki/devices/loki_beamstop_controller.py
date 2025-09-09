2814ffaf9a99ba464d478e82afe92cfef00445b6
linemoller <line.moller@ess.eu>
2025-09-09 13:19:10 +0200

    make function for duplicated method, change doStart to start and doRead to read inside the functions

--- nicos_ess/loki/devices/loki_beamstop_controller.py
+++ nicos_ess/loki/devices/loki_beamstop_controller.py
@@ -8,25 +8,29 @@ class LokiBeamstopController(MultiTargetMapping):
     def doStart(self, value):
         active_beamstop = self._get_active_beamstop()
         if not (active_beamstop in value):
-            self._park_all_beamstops()
-        self._move_beamstops_to_new_target(value)
+            self.park_all_beamstops()
+        self._move_beamstops(value)
+
+    def park_all_beamstops(self):
+        """
+        The default target mapping must be parking positions for all beamstop motors.
+        """
+        targets = self.mapping.get(self.default_target, None)
+        self._move_to_targets(targets)
+
+    def _move_beamstops(self, value):
+        targets = self.mapping.get(value, None)
+        self._move_to_targets(targets)
+
+    def _move_to_targets(self, targets):
+        for device, target in zip(self._attached_controlled_devices, targets):
+            device.start(target)
+            waitForCompletion(device)
 
     def _get_active_beamstop(self):
-        cur_value = self.doRead()
+        cur_value = self.read()
         active_beamstop_match = re.match(r"Beamstop \d", cur_value)
         if active_beamstop_match:
             return active_beamstop_match.group()
         else:
             return "None"
-
-    def _park_all_beamstops(self):
-        targets = self.mapping.get(self.default_target, None)
-        for channel, target in zip(self._attached_controlled_devices, targets):
-            channel.start(target)
-            waitForCompletion(channel)
-
-    def _move_beamstops_to_new_target(self, value):
-        targets = self.mapping.get(value, None)
-        for channel, target in zip(self._attached_controlled_devices, targets):
-            channel.doStart(target)
-            waitForCompletion(channel)
