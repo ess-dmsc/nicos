3f1d6949487adaa0e5eac782e34f08c9b161075b
Jonas Petersson <jonas.petersson@ess.eu>
2025-09-12 13:58:38 +0200

    ECDC-5033: Add important vacuum signals

--- nicos_ess/bifrost/setups/vacuum.py
+++ nicos_ess/bifrost/setups/vacuum.py
@@ -1,4 +1,4 @@
-description = "Vacuum status for bunker and instrument zone"
+description = "Vacuum status for BIFROST"
 
 BUNKER_ROOT = "BIFRO-VacBnkr:Vac-"
 INSTRUMENT_ROOT = "BIFRO-VacInstr:Vac-"
@@ -35,15 +35,41 @@ def make_key(zone: str, tag: str) -> str:
     return f"{zone}_vacuum_{tag.lower().replace('-', '')}"
 
 
-devices = {}
+# Commenting out code = bad practice - but I'll do it here in case they want all these signals again
+# devices = {}
+# for zone, root, tags in [
+#     ("bunker", BUNKER_ROOT, BUNKER_GAUGES),
+#     ("instrument", INSTRUMENT_ROOT, INSTRUMENT_GAUGES),
+# ]:
+#     for tag in tags:
+#         devices[make_key(zone, tag)] = device(
+#             "nicos_ess.devices.epics.pva.EpicsReadable",
+#             description=f"{zone.capitalize()} zone vacuum gauge {tag.split('-')[-1]}",
+#             readpv=f"{root}{tag}:PrsR",
+#         )
 
-for zone, root, tags in [
-    ("bunker", BUNKER_ROOT, BUNKER_GAUGES),
-    ("instrument", INSTRUMENT_ROOT, INSTRUMENT_GAUGES),
-]:
-    for tag in tags:
-        devices[make_key(zone, tag)] = device(
-            "nicos_ess.devices.epics.pva.EpicsReadable",
-            description=f"{zone.capitalize()} zone vacuum gauge {tag.split('-')[-1]}",
-            readpv=f"{root}{tag}:PrsR",
-        )
+
+devices = dict(
+    tank_pressure=device(
+        "nicos_ess.devices.epics.pva.EpicsReadable",
+        description="Instrument zone tank vacuum pressure",
+        readpv="BIFRO-VacInstr:Vac-VGP-031:PrsR",
+        fmtstr="%.2e",
+    ),
+    be_filter_pressure=device(
+        "nicos_ess.devices.epics.pva.EpicsReadable",
+        description="Instrument zone Be filter vacuum pressure",
+        readpv="BIFRO-VacInstr:Vac-VGF-032:PrsR",
+        fmtstr="%.2e",
+    ),
+    be_filter_pressure_ok=device(
+        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
+        description="Instrument zone Be filter vacuum pressure OK status. set is OK",
+        readpv="BIFRO-VacInstr:Vac-VGF-032:Rly2Stat-R",
+    ),
+    backing_pump_interlock=device(
+        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
+        description="Instrument zone Be filter valve interlock status. HEALTHY is OK",
+        readpv="BIFRO-VacInstr:Vac-VVA-032:ITLckStatR",
+    ),
+)
