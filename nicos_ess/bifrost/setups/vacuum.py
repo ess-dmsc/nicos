7c15f01ac24055b9a74452b787f1c3cb52ad350c
Jonas Petersson <jonas.petersson@ess.eu>
2025-09-12 13:40:37 +0200

    change name for interlock

--- nicos_ess/bifrost/setups/vacuum.py
+++ nicos_ess/bifrost/setups/vacuum.py
@@ -67,7 +67,7 @@ devices = dict(
         description="Instrument zone Be filter vacuum pressure OK status. set is OK",
         readpv="BIFRO-VacInstr:Vac-VGF-032:Rly2Stat-R",
     ),
-    be_filter_valve_interlock=device(
+    backing_pump_interlock=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Instrument zone Be filter valve interlock status. HEALTHY is OK",
         readpv="BIFRO-VacInstr:Vac-VVA-032:ITLckStatR",
