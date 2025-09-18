268aad5b03e8355cb8e1e717cebc5d4c34679359
Jonas Petersson <jonas.petersson@ess.eu>
2025-09-18 17:03:59 +0200

    not thinking enough

--- nicos_ess/devices/epics/chopper.py
+++ nicos_ess/devices/epics/chopper.py
@@ -280,7 +280,7 @@ class OdinChopperController(EpicsParameters, MappedMoveable):
 
     def doRead(self, maxage=0):
         return get_from_cache_or(
-            "state",
+            self,
             self._record_fields["state"].cache_key,
             lambda: self._epics_wrapper.get_value(f"{self.pv_root}ChopState_R"),
         )
