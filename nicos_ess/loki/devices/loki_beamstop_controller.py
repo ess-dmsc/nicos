29eeeebeafcca742d4f545d5ca7fe904eebb659b
linemoller <line.moller@ess.eu>
2025-09-22 12:22:47 +0200

    add parameter from multitargetmapping

--- nicos_ess/loki/devices/loki_beamstop_controller.py
+++ nicos_ess/loki/devices/loki_beamstop_controller.py
@@ -1,10 +1,20 @@
 import re
 
+from nicos.core import Param
 from nicos.core.utils import waitForCompletion
 from nicos_ess.devices.mapped_controller import MultiTargetMapping
 
 
 class LokiBeamstopController(MultiTargetMapping):
+    parameters = {
+        "all_parked_mapping": Param(
+            mandatory=True,
+            settable=True,
+            description="The mapped value that corresponds to all motors parked",
+            type=str,
+        )
+    }
+
     def doStart(self, value):
         active_beamstop = self._extract_beamstop_number(self.read())
         requested_beamstop = self._extract_beamstop_number(value)
@@ -13,10 +23,7 @@ class LokiBeamstopController(MultiTargetMapping):
         self._move_beamstops(value)
 
     def park_all_beamstops(self):
-        """
-        The default target mapping must be parking positions for all beamstop motors.
-        """
-        targets = self.mapping.get(self.default_target, None)
+        targets = self.mapping.get(self.all_parked_mapping, None)
         self._move_to_targets(targets)
 
     def _extract_beamstop_number(self, value):
