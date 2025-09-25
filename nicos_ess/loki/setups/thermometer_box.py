443d318307f615e70f3debf092c1edc96bd7ee93
linemoller <line.moller@ess.eu>
2025-09-25 13:12:36 +0200

    rename temp devices to match hardware

--- nicos_ess/loki/setups/thermometer_box.py
+++ nicos_ess/loki/setups/thermometer_box.py
@@ -6,7 +6,7 @@ devices = dict()
 
 num_cells = 8
 for i in range(1, num_cells + 1):
-    devices[f"temp_{i}"] = device(
+    devices[f"ts{i}"] = device(
         "nicos_ess.devices.epics.pva.EpicsReadable",
         description=f"Temperature sensor {i}.",
         readpv=f"{pv_root}PT100_TS{i}",
