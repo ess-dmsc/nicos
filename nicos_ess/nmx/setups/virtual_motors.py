3147a5d20ca498f4cb6a004d83cd719e6be86b8a
Lais Pessine do Carmo <lais.pessine@ess.eu>
2025-10-28 16:50:57 +0100

    NMX: Update virtual motors (names and settings)

--- nicos_ess/nmx/setups/virtual_motors.py
+++ nicos_ess/nmx/setups/virtual_motors.py
@@ -1,33 +1,30 @@
-description = "Simulated sample motors (mockup)"
+description = "Virtual motors (for testing/debugging)"
 
 group = "optional"
 
 devices = dict(
-    sim_motor_1=device(
+    virtual_motor_1=device(
         "nicos.devices.generic.VirtualMotor",
-        description="Sim motor 1 (mockup)",
+        description="Virtual motor 1",
         fmtstr="%.2f",
         unit="mm",
         speed=0.5,
         abslimits=(-10000, 10000),
-        visibility=(),
     ),
-    sim_motor_2=device(
+    virtual_motor_2=device(
         "nicos.devices.generic.VirtualMotor",
-        description="Sim motor 2 (mockup)",
+        description="Virtual motor 2",
         fmtstr="%.2f",
         unit="mm",
         speed=0.5,
         abslimits=(-10000, 10000),
-        visibility=(),
     ),
-    sim_motor_3=device(
+    virtual_motor_3=device(
         "nicos.devices.generic.VirtualMotor",
-        description="Sim motor 3 (mockup)",
+        description="Virtual motor 3",
         fmtstr="%.2f",
         unit="mm",
         speed=0.5,
         abslimits=(-10000, 10000),
-        visibility=(),
     ),
 )
