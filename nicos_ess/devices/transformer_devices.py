d5d06e50162629f218d9df160072b160b7c37b62
linemoller <line.moller@ess.eu>
2025-11-05 11:07:50 +0100

    add limits from attached motor to transformer device

--- nicos_ess/devices/transformer_devices.py
+++ nicos_ess/devices/transformer_devices.py
@@ -1,4 +1,4 @@
-from nicos.core import Attach, Moveable, Param, multiStatus
+from nicos.core import Attach, HasLimits, Moveable, Override, Param, multiStatus
 from nicos.devices.abstract import TransformedMoveable
 from nicos_ess.devices.epics.pva import EpicsManualMappedAnalogMoveable
 from nicos_ess.devices.epics.pva.motor import EpicsJogMotor
@@ -66,7 +66,7 @@ class ChopperPhase(TransformedMoveable):
         return abs(self._attached_mapped_speed_dev.mapping.get(target_val))
 
 
-class DegreesPerSecondToRPM(TransformedMoveable):
+class DegreesPerSecondToRPM(HasLimits, TransformedMoveable):
     parameters = {
         "unit": Param(
             description="Converts unit of attached motor from degrees/sec to RPM",
@@ -76,9 +76,30 @@ class DegreesPerSecondToRPM(TransformedMoveable):
         )
     }
 
+    parameter_overrides = {
+        "abslimits": Override(volatile=True, mandatory=False),
+        "userlimits": Override(volatile=True, chatty=False),
+    }
+
     attached_devices = {
         "motor": Attach("The attached motor", EpicsJogMotor, optional=False)
     }
+
+    def doReadAbslimits(self):
+        raw_absmin, raw_absmax = self._attached_motor.doReadAbslimits()
+        absmin = self._convert_degrees_per_second_to_rpm(raw_absmin)
+        absmax = self._convert_degrees_per_second_to_rpm(raw_absmax)
+        return absmin, absmax
+
+    def doReadUserlimits(self):
+        raw_umin, raw_umax = self._attached_motor.doReadUserlimits()
+        umin = self._convert_degrees_per_second_to_rpm(raw_umin)
+        umax = self._convert_degrees_per_second_to_rpm(raw_umax)
+        return umin, umax
+
+    def doWriteUserlimits(self, value):
+        self._attached_motor.doWriteUserlimits(value)
+        return self.userlimits
 
     def _convert_degrees_per_second_to_rpm(self, value):
         return (value / 360) * 60
