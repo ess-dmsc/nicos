2e7455ff3091737bf0b66b99648bb3150c3930aa
Renee Helfert <renee.helfert@ess.eu>
2025-11-06 09:32:02 +0100

    fix: small change to pv root

--- nicos_ess/estia/setups/motion_cabinet_1.py
+++ nicos_ess/estia/setups/motion_cabinet_1.py
@@ -1,12 +1,10 @@
 description = "Motion Cabinet 1"
 
-root = "ESTIA-MCS1:MC-MCU-01:"
-
 devices = dict(
     cabinet_1_status=device(
         "nicos_ess.devices.epics.mbbi_direct.MBBIDirectStatus",
         description="Cabinet 1 Status",
-        pv_root=f"{root}Cabinet",
+        pv_root="ESTIA-MCS1:MC-MCU-01:Cabinet",
         number_of_bits=24,
     ),
 )
--- nicos_ess/estia/setups/motion_cabinet_2.py
+++ nicos_ess/estia/setups/motion_cabinet_2.py
@@ -1,12 +1,10 @@
 description = "Motion Cabinet 2"
 
-root = "ESTIA-MCS2:MC-MCU-02:"
-
 devices = dict(
     cabinet_1_status=device(
         "nicos_ess.devices.epics.mbbi_direct.MBBIDirectStatus",
         description="Cabinet 2 Status",
-        pv_root=f"{root}Cabinet",
+        pv_root="ESTIA-MCS2:MC-MCU-02:Cabinet",
         number_of_bits=24,
     ),
 )
