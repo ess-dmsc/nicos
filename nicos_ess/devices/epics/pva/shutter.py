4c98d58dbd541116ccd2d504053a4bf51c90654f
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2025-12-09 11:05:37 +0100

    Add subscription callbacks

--- nicos_ess/devices/epics/pva/shutter.py
+++ nicos_ess/devices/epics/pva/shutter.py
@@ -1,3 +1,5 @@
+import time
+
 from nicos import session
 from nicos.core import (
     POLLER,
@@ -13,6 +15,25 @@ from nicos.devices.epics.pva import (
     EpicsMappedMoveable,
     EpicsMappedReadable,
 )
+
+
+def get_from_cache_or(device, cache_key, func):
+    if device.monitor:
+        result = device._cache.get(device._name, cache_key)
+        if result is not None:
+            return result
+    return func()
+
+
+def _update_mapped_choices(mapped_device):
+    choices = mapped_device._epics_wrapper.get_value_choices(mapped_device.readpv)
+    new_mapping = {}
+    for i, choice in enumerate(choices):
+        new_mapping[choice] = i
+    mapped_device._setROParam("mapping", new_mapping)
+    mapped_device._inverse_mapping = {}
+    for k, v in mapped_device.mapping.items():
+        mapped_device._inverse_mapping[v] = k
 
 
 class EpicsShutter(EpicsMappedMoveable):
@@ -56,6 +77,32 @@ class EpicsShutter(EpicsMappedMoveable):
         if mode == SIMULATION:
             return
 
+        if session.sessiontype == POLLER and self.monitor:
+            self._epics_subscriptions.append(
+                self._epics_wrapper.subscribe(
+                    self.readpv,
+                    "value",
+                    self._value_change_callback,
+                    self._connection_change_callback,
+                )
+            )
+            self._epics_subscriptions.append(
+                self._epics_wrapper.subscribe(
+                    self.readpv,
+                    "value",
+                    self._status_change_callback,
+                    self._connection_change_callback,
+                )
+            )
+            self._epics_subscriptions.append(
+                self._epics_wrapper.subscribe(
+                    self.writepv,
+                    "target",
+                    self._value_change_callback,
+                    self._connection_change_callback,
+                )
+            )
+
         EpicsDevice.doInit(self, mode)
 
         if session.sessiontype != POLLER:
@@ -77,15 +124,6 @@ class EpicsShutter(EpicsMappedMoveable):
         else:
             self.log.warn("Reset isn't available on device or the resetpv is missing")
 
-    def doReadClosingbit(self):
-        return self._epics_wrapper.get_pv_value(self.closingbit, as_string=False)
-
-    def doReadOpeningbit(self):
-        return self._epics_wrapper.get_pv_value(self.openingbit, as_string=False)
-
-    def doReadMsgTxt(self):
-        return self._epics_wrapper.get_pv_value(self.msgtxt, as_string=True)
-
     def doStatus(self, maxage=0):
         try:
             severity, msg = self._epics_wrapper.get_alarm_status(self.readpv)
@@ -96,6 +134,51 @@ class EpicsShutter(EpicsMappedMoveable):
         if self.doReadClosingbit() == 1 or self.doReadOpeningbit() == 1:
             return status.BUSY, self.doReadMsgTxt()
         return status.OK, ""
+
+    def _value_change_callback(
+        self, name, param, value, units, limits, severity, message, **kwargs
+    ):
+        if name not in {self.readpv, self.writepv, self.targetpv}:
+            # Unexpected updates ignored
+            return
+        time_stamp = time.time()
+        if name == self.readpv:
+            if not self.mapping:
+                _update_mapped_choices(self)
+            self._cache.put(
+                self._name,
+                param,
+                self._inverse_mapping.get(value, value),
+                time_stamp,
+            )
+            self._cache.put(self._name, "unit", units, time_stamp)
+        if name == self.writepv and not self.target:
+            self._cache.put(self._name, param, value, time_stamp)
+        if name == self.targetpv:
+            self._cache.put(self._name, param, value, time_stamp)
+
+    def _status_change_callback(
+        self, name, param, value, units, limits, severity, message, **kwargs
+    ):
+        if name != self.readpv:
+            # Unexpected updates ignored
+            return
+        self._cache.put(self._name, "status", (severity, message), time.time())
+
+    def _connection_change_callback(self, name, param, is_connected, **kwargs):
+        if param != "value":
+            return
+
+        if is_connected:
+            self.log.debug("%s connected!", name)
+        else:
+            self.log.warning("%s disconnected!", name)
+            self._cache.put(
+                self._name,
+                "status",
+                (status.ERROR, "communication failure"),
+                time.time(),
+            )
 
 
 class EpicsHeavyShutter(EpicsMappedReadable):
