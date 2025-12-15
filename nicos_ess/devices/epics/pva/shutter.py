2a36170fee6068f641a9dabbe80a3ab941463397
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2025-12-15 14:15:08 +0100

    Consolidate different varieties of _update_mapped_choices

--- nicos_ess/devices/epics/pva/shutter.py
+++ nicos_ess/devices/epics/pva/shutter.py
@@ -3,43 +3,19 @@ import time
 from nicos import session
 from nicos.core import (
     POLLER,
-    SIMULATION,
     Param,
     none_or,
     pvname,
     status,
 )
 from nicos.devices.abstract import MappedMoveable
-from nicos.devices.epics.pva import (
-    EpicsDevice,
-    EpicsMappedReadable,
-)
+from nicos.devices.epics.pva import EpicsMappedReadable
 from nicos_ess.devices.epics.pva.epics_devices import (
     EpicsMappedMoveable,
+    _update_mapped_choices,
     get_from_cache_or,
+    pvReadOrWrite,
 )
-
-
-def _update_mapped_choices_from_readpv(mapped_device):
-    choices = mapped_device._epics_wrapper.get_value_choices(mapped_device.readpv)
-    new_mapping = {}
-    for i, choice in enumerate(choices):
-        new_mapping[choice] = i
-    mapped_device._setROParam("mapping", new_mapping)
-    mapped_device._inverse_mapping = {}
-    for k, v in mapped_device.mapping.items():
-        mapped_device._inverse_mapping[v] = k
-
-
-def _update_mapped_choices_from_writepv(mapped_device):
-    choices = mapped_device._epics_wrapper.get_value_choices(mapped_device.writepv)
-    new_mapping = {}
-    for i, choice in enumerate(choices):
-        new_mapping[choice] = i
-    mapped_device._setROParam("mapping", new_mapping)
-    mapped_device._inverse_mapping = {}
-    for k, v in mapped_device.mapping.items():
-        mapped_device._inverse_mapping[v] = k
 
 
 class EpicsShutter(EpicsMappedMoveable):
@@ -77,7 +53,7 @@ class EpicsShutter(EpicsMappedMoveable):
 
     def doInit(self, mode):
         if session.sessiontype == POLLER and self.monitor:
-            _update_mapped_choices_from_readpv(self)
+            _update_mapped_choices(self, pvReadOrWrite.readpv)
 
             self._epics_subscriptions.append(
                 self._epics_wrapper.subscribe(
@@ -114,7 +90,7 @@ class EpicsShutter(EpicsMappedMoveable):
                 )
 
         if session.sessiontype != POLLER:
-            _update_mapped_choices_from_writepv(self)
+            _update_mapped_choices(self, pvReadOrWrite.writepv)
         MappedMoveable.doInit(self, mode)
 
     def _is_closing(self):
@@ -141,28 +117,6 @@ class EpicsShutter(EpicsMappedMoveable):
 
     def doStatus(self, maxage=0):
         return get_from_cache_or(self, "status", self._do_status)
-
-    def _value_change_callback(
-        self, name, param, value, units, limits, severity, message, **kwargs
-    ):
-        if name not in {self.readpv, self.writepv, self.targetpv}:
-            # Unexpected updates ignored
-            return
-        time_stamp = time.time()
-        if name == self.readpv:
-            if not self.mapping:
-                _update_mapped_choices_from_writepv(self)
-            self._cache.put(
-                self._name,
-                param,
-                self._inverse_mapping.get(value, value),
-                time_stamp,
-            )
-            self._cache.put(self._name, "unit", units, time_stamp)
-        if name == self.writepv and not self.target:
-            self._cache.put(self._name, param, value, time_stamp)
-        if name == self.targetpv:
-            self._cache.put(self._name, param, value, time_stamp)
 
     def _status_change_callback(
         self, name, param, value, units, limits, severity, message, **kwargs
