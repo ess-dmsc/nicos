ee3f95db634e19fb0dcbd6b6ec19512af9f74299
linemoller <line.moller@ess.eu>
2025-12-22 10:24:33 +0100

    add missing mapping

--- test/setups/custom/ess_power_supply.py
+++ test/setups/custom/ess_power_supply.py
@@ -9,5 +9,6 @@ devices = dict(
     ps_bank_hv=device(
         "test.nicos_ess.test_devices.test_power_supply.FakePowerSupplyBank",
         ps_channels=["ps_channel_1"],
+        mapping={"OFF": 0, "ON": 1},
     ),
 )
