7fe62008757495dc591159a14ded75baa1f8364a
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2026-02-05 14:42:27 +0100

    Estia slit focus mask

---
+++ nicos_ess/estia/setups/sample_slit.py
@@ -0,0 +1,38 @@
+description = "The slit arrangement with 4 motors"
+prefix = "ESTIA-SpSl:MC-Sl"
+
+devices = dict(
+    blade_l=device(
+        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+        description="Left blade",
+        motorpv=f"{prefix}Yp:Mtr",
+        visibility=(),
+    ),
+    blade_r=device(
+        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+        description="Right blade",
+        motorpv=f"{prefix}Ym:Mtr",
+        visibility=(),
+    ),
+    blade_t=device(
+        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+        description="Top blade",
+        motorpv=f"{prefix}Zp:Mtr",
+        visibility=(),
+    ),
+    blade_b=device(
+        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+        description="Bottom blade",
+        motorpv=f"{prefix}Zm:Mtr",
+        visibility=(),
+    ),
+    slit_1=device(
+        "nicos.devices.generic.slit.Slit",
+        description="Slit 1 with left, right, bottom and top motors",
+        opmode="centered",
+        left="blade_l",
+        right="blade_r",
+        top="blade_t",
+        bottom="blade_b",
+    ),
+)
