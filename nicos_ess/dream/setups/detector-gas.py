71f47c1f604a2ec068c66e09b56f0387d48b22a8
George Nicolas Kontogiorgos <george.kontogiorgos@ess.eu>
2026-02-11 16:32:27 +0100

    dream, detector gas: fix units

--- nicos_ess/dream/setups/detector-gas.py
+++ nicos_ess/dream/setups/detector-gas.py
@@ -5,15 +5,18 @@ devices = dict(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Gas pressure of left detector bundle",
         readpv="DREAM-DtCmn:GaD-001:AI0User",
+        units="bar",
     ),
     bundle_right_pressure=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Gas pressure of right detector bundle",
         readpv="DREAM-DtCmn:GaD-001:AI1User",
+        units="bar",
     ),
     cave_pressure=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Gas pressure in detector cave",
         readpv="DREAM-DtCmn:GaD-001:AI3User",
+        units="bar",
     ),
 )
