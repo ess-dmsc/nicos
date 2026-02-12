e86543c2417a2de78889f9f76f89743892cc11b4
linemoller <line.moller@ess.eu>
2026-02-12 11:42:10 +0100

    update pv root

--- nicos_ess/nido/setups/cetoni_pump.py
+++ nicos_ess/nido/setups/cetoni_pump.py
@@ -4,6 +4,7 @@ devices = dict(
     pump_1=device(
         "nicos_ess.loki.devices.cetoni_pump.CetoniPumpController",
         description="Description",
-        pvroot="B02-CSLab:SE-Pumps:",
+        pvroot="B02-CSLab:SE-Pumps:SP1",
+        innerdiameter="TBD",
     ),
 )
