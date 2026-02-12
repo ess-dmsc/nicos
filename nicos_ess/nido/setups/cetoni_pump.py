319f774e334db8e6f9f303a3b50da42a1ef18b11
linemoller <line.moller@ess.eu>
2026-02-12 09:36:38 +0100

    add setup file

--- nicos_ess/nido/setups/cetoni_pump.py
+++ nicos_ess/nido/setups/cetoni_pump.py
@@ -1,17 +1,9 @@
 description = "The cetoni pumps"
 
-pump1_pvroot = "B02-CSLab:SE-Pumps:SP1"
-
 devices = dict(
-    pump1=device(
+    pump_1=device(
         "nicos_ess.loki.devices.cetoni_pump.CetoniPumpController",
         description="Description",
-        pvroot=pump1_pvroot,
-    ),
-    pump1_syringe_type=device(
-        "nicos_ess.epics.pva.EpicsMappedMoveable",
-        description="Description",
-        readpv=f"{pump1_pvroot}SyrType-SP",
-        writepv=f"{pump1_pvroot}SyrType-SP",
+        pvroot="B02-CSLab:SE-Pumps:",
     ),
 )
