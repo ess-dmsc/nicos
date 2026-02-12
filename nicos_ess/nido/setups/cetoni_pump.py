855c2cbed282cf473107e3f0f50749b49b6956ca
linemoller <line.moller@ess.eu>
2026-02-12 09:36:38 +0100

    add setup file

---
+++ nicos_ess/nido/setups/cetoni_pump.py
@@ -0,0 +1,9 @@
+description = "The cetoni pumps"
+
+devices = dict(
+    pump_1=device(
+        "nicos_ess.loki.devices.cetoni_pump.CetoniPumpController",
+        description="Description",
+        pvroot="B02-CSLab:SE-Pumps:",
+    ),
+)
