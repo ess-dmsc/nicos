5d208a0c9aeec5e628b6b0dc24dae1f42285d434
linemoller <line.moller@ess.eu>
2026-02-17 14:40:14 +0100

    try dont rely on cache

--- nicos_ess/loki/devices/cetoni_pump.py
+++ nicos_ess/loki/devices/cetoni_pump.py
@@ -146,11 +146,12 @@ class CetoniPumpController(EpicsParameters, CanReference, HasLimits, Moveable):
         return self._get_cached_pv_or_ask("readpv")
 
     def doStart(self, value):
-        self._cache.put(self, "status", (status.BUSY, "Moving"), time.time())
+        # self._cache.put(self, "status", (status.BUSY, "Moving"), time.time())
         self._set_pv(self._get_pv_name("writepv"), value)
 
     def doStatus(self, maxage=0):
-        return get_from_cache_or(self, "status", self._do_status)
+        # return get_from_cache_or(self, "status", self._do_status)
+        return self._do_status(maxage=0)
 
     def _do_status(self, maxage=0):
         fault = self._get_cached_pv_or_ask("isfault")
