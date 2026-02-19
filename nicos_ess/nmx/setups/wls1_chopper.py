4ae400ef0f050880d9b9a149749c3879f845e9b0
George Nicolas Kontogiorgos <george.kontogiorgos@ess.eu>
2026-02-19 16:24:37 +0100

    nmx, choppers: add motor temperature, log output and levitation status

--- nicos_ess/nmx/setups/wls1_chopper.py
+++ nicos_ess/nmx/setups/wls1_chopper.py
@@ -5,6 +5,24 @@ pv_root_1 = "NMX-ChpSy1:Chop-WLS-101:"
 chic_root = "NMX-ChpSy1:Chop-CHIC-001:"
 
 devices = dict(
+    wls1_log=device(
+        "nicos_ess.devices.epics.pva.EpicsStringReadable",
+        description="The logs from chopper controller",
+        readpv="{}Log_R".format(pv_root_1),
+        visibility=(),
+    ),
+    wls1_levitation_status=device(
+        "nicos_ess.devices.epics.pva.EpicsMappedReadable",
+        description="The chopper status.",
+        readpv="{}LeviStatus_R".format(pv_root_1),
+        visibility=(),
+    ),
+    wls1_motor_temperature=device(
+        "nicos_ess.devices.epics.pva.EpicsReadable",
+        description="The temperature of the motor of the chopper",
+        readpv="{}MtrTemp_R".format(pv_root_1),
+        visibility=(),
+    ),
     wls1_status=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="The chopper status.",
