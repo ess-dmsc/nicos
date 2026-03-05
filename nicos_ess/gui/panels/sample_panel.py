bc8f7cc9e20c06da0cab208a32012135d1a3f68e
linemoller <line.moller@ess.eu>
2026-03-05 09:39:40 +0100

    refactor: split up build_ui

--- nicos_ess/gui/panels/sample_panel.py
+++ nicos_ess/gui/panels/sample_panel.py
@@ -33,20 +33,59 @@ class SamplePanel(PanelBase):
         self.options = options
 
         self.sample_table = self.create_table_data()
-        self.build_ui()
+        self._build_ui()
 
     def create_table_data(self):
         samples = {key: "" for key in SAMPLE_FIELDS}
         return samples
 
-    def build_ui(self):
-        layout = QVBoxLayout(self)
+    def _build_ui(self):
+        self._create_widgets()
+        self._set_up_layout()
+        self._prepare_table()
+
+    def _create_widgets(self):
+        self.label = QLabel("Sample information")
+        self.table = QTableWidget()
+        self.button_add = QPushButton("Add")
+        self.button_delete = QPushButton("Delete")
+        self.button_apply = QPushButton("Apply")
+        self.button_discard = QPushButton("Discard changes")
+        self.edits_warning = QLabel(
+            "Changes to the experiment proposal have not been applied!"
+        )
+
+    def _set_up_layout(self):
+        table_layout = QVBoxLayout()
+        table_layout.addWidget(self.table)
+        table_layout.addStretch(2)
+
+        button_layout = QVBoxLayout()
+        button_layout.addWidget(self.button_add)
+        button_layout.addWidget(self.button_delete)
+        button_layout.addStretch()
 
         top_layout = QHBoxLayout()
-        self.label = QLabel("Sample information")
+        top_layout.addWidget(self.label, alignment=QtCore.Qt.AlignTop)
+        top_layout.addLayout(table_layout)
+        top_layout.addLayout(button_layout)
 
-        table_layout = QVBoxLayout()
-        self.table = QTableWidget()
+        right_layout = QVBoxLayout()
+        right_layout.addWidget(self.edits_warning)
+        right_layout.addWidget(self.button_discard, alignment=Qt.AlignRight)
+
+        bottom_layout = QHBoxLayout()
+        bottom_layout.addWidget(self.button_apply)
+        bottom_layout.addStretch()
+        bottom_layout.addLayout(right_layout)
+
+        layout = QVBoxLayout(self)
+        layout.addLayout(top_layout)
+        layout.addLayout(bottom_layout)
+
+        self.setLayout(layout)
+
+    def _prepare_table(self):
         for i, key in enumerate(self.sample_table.keys()):
             self.table.insertRow(i)
             self.table.setVerticalHeaderItem(i, QTableWidgetItem(key))
@@ -55,35 +94,3 @@ class SamplePanel(PanelBase):
         self.table.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
         self.table.horizontalHeader().setStretchLastSection(True)
         self.table.verticalHeader().setStretchLastSection(False)
-        table_layout.addWidget(self.table)
-        table_layout.addStretch(2)
-
-        button_layout = QVBoxLayout()
-        self.button_add = QPushButton("Add")
-        self.button_delete = QPushButton("Delete")
-        button_layout.addWidget(self.button_add)
-        button_layout.addWidget(self.button_delete)
-        button_layout.addStretch()
-
-        top_layout.addWidget(self.label, alignment=QtCore.Qt.AlignTop)
-        top_layout.addLayout(table_layout)
-        top_layout.addLayout(button_layout)
-
-        bottom_layout = QHBoxLayout()
-        self.button_apply = QPushButton("Apply")
-        self.button_discard = QPushButton("Discard changes")
-        self.edits_warning = QLabel(
-            "Changes to the experiment proposal have not been applied!"
-        )
-
-        right_layout = QVBoxLayout()
-        right_layout.addWidget(self.edits_warning)
-        right_layout.addWidget(self.button_discard, alignment=Qt.AlignRight)
-        bottom_layout.addWidget(self.button_apply)
-        bottom_layout.addStretch()
-        bottom_layout.addLayout(right_layout)
-
-        layout.addLayout(top_layout)
-        layout.addLayout(bottom_layout)
-
-        self.setLayout(layout)
