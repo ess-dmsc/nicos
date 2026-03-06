e5365942da8e7eefb4376e1b6a02efa9c23fb138
Hanno Perrey <hanno.perrey@ess.eu>
2026-03-06 09:19:17 +0100

    style(core/cfg): Unnecessary use of comprehension

--- nicos/configmod.py
+++ nicos/configmod.py
@@ -171,7 +171,7 @@ def readConfig():
         # Iterate from least specific (site) to most specific (user) until a
         # configuration file is found
         dirs = PlatformDirs("nicos", "nicos")
-        for config_dir in reversed([d for d in dirs.iter_config_paths()]):
+        for config_dir in reversed(list(dirs.iter_config_paths())):
             config_file = config_dir / nicos_cfg_fn
             print(f"Looking for configuration file in {config_file}")
             if config_file.exists():
