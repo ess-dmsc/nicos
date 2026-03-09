7edc6bb97dc028eff642465839ceb30d5424011e
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2026-03-09 13:17:17 +0100

    ESTIA: Fix polarizer motorpv parameter name in setup

--- nicos_ess/estia/setups/polarizer.py
+++ nicos_ess/estia/setups/polarizer.py
@@ -4,11 +4,11 @@ devices = dict(
     in_beam_changer=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Polarizer in-beam changer",
-        pv_root="ESTIA-PolChg:MC-LinY01:Mtr",
+        motorpv="ESTIA-PolChg:MC-LinY01:Mtr",
     ),
     angular_adjustment=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Polarizer angular adjustment",
-        pv_root="ESTIA-PolChg:MC-RotZ01:Mtr",
+        motorpv="ESTIA-PolChg:MC-RotZ01:Mtr",
     ),
 )
