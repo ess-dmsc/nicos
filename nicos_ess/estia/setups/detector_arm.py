ddd7d27d99e3539ee4558c5d69fdcb46eead91d7
Hanno Perrey <hanno.perrey@ess.eu>
2026-03-16 14:31:58 +0100

    feat(estia/setups): add detector arm

---
+++ nicos_ess/estia/setups/detector_arm.py
@@ -0,0 +1,19 @@
+description = "Motors for the detector arm"
+
+det_arm_root = "ESTIA-DtCpl:MC-Pne-01"
+
+devices = dict(
+    detector_rot=device(
+        "nicos_ess.devices.epics.pva.motor.EpicsMotor",
+        description="Detector Motion System (Z-Rotation)",
+        motorpv="ESTIA-DtRot:MC-RotZ01",
+    ),
+    detector_arm=device(
+        "nicos_ess.devices.epics.pva.shutter.EpicsShutter",
+        description="Detector Arm Pneumatic Coupling: Support Structure on Air Pads",
+        writepv=f"{det_arm_root}:ShtOpen",
+        readpv=f"{det_arm_root}:ShtAuxBits07",
+        resetpv=f"{det_arm_root}:ShtErrRst",
+        msgtxt=f"{det_arm_root}:ShtMsgTxt",
+    ),
+)
