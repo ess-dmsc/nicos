ee20eb6eb68161921552fbbe7f1d8f1abd7a5e0c
Renee Helfert <renee.helfert@ess.eu>
2026-03-18 10:57:51 +0100

    ESTIA - Virtual Source Update

--- nicos_ess/estia/devices/virtual_source.py
+++ nicos_ess/estia/devices/virtual_source.py
@@ -1,118 +1,17 @@
-import numpy as np
-
 from nicos.core import (
     Attach,
     Moveable,
     Override,
     Param,
-    Readable,
     Value,
     dictof,
     multiStatus,
     oneof,
-    status,
     tupleof,
 )
 
 
-class VSCalculator(Readable):
-    """Readout Calculator for the Virtual Source Slit System
-
-    The slit consists of two L-shaped blades controlled by 5 motors:
-    - 2 motions along the x-axis
-    - 2 motions along the y-axis
-    - 1 shared rotational motion along the z-axis
-
-    Axis stated are defined via the right-hand rule with +x heading down the beamline towards the sample
-
-    The vertical height of the slit is determined by standard slit motions, however
-    the width is determined by the gap distance between the blades along the x-axis
-    and a shared rotation around the z-axis.
-    """
-
-    parameters = {
-        "fmtstr_map": Param(
-            "A dictionary mapping operation modes to format strings (used for internal management).",
-            type=dictof(str, str),
-            settable=False,
-            mandatory=False,
-            userparam=False,
-            default={
-                "4blades": "(%.3f mm x %.3f mm) %.3f deg",
-                "centered": "(%.3f mm x %.3f mm) %.3f deg",
-            },
-        ),
-        "opmode": Param(
-            "Mode of operation",
-            type=oneof("4blades", "centered"),
-            settable=True,
-            default="4blades",
-        ),
-    }
-
-    parameter_overrides = {
-        "unit": Override(default="", mandatory=False, settable=True),
-    }
-    valuetype = tupleof(float, float)
-
-    attached_devices = {
-        "slit": Attach("the slit blades", Moveable),
-        "rot": Attach("the rotation stage", Moveable),
-    }
-
-    def _findGap(self, pos, angle):
-        # [-left, +right, -bottom, +top]
-        l, r, b, t = pos
-        height = t - b
-
-        rad = np.deg2rad(angle)
-
-        left_gap = float(-(l * np.sin(rad)))
-        right_gap = float(r * np.sin(rad))
-        width = left_gap + right_gap
-
-        return [width, height]
-
-    def _doReadPositions(self, maxage):
-        positions = self._adevs["slit"]._doReadPositions(maxage)
-        angle = self._adevs["rot"].read(maxage)
-
-        width, height = self._findGap(positions, angle)
-        return [width, height, angle]
-
-    def doRead(self, maxage=0):
-        # reader opmode should take prioriy over slit opmode
-        self._syncOpmode(self._adevs["slit"].opmode, self.opmode)
-        return self._doReadPositions(maxage)
-
-    def doStatus(self, maxage=0):
-        return status.OK, f"{self.opmode} mode"
-
-    def doSetPosition(self, pos):
-        pass
-
-    def doWriteFmtstr(self, value):
-        # since self.fmtstr_map is a readonly dict a temp. copy is created
-        # to update the dict and then put to cache back
-        tmp = dict(self.fmtstr_map)
-        tmp[self.opmode] = value
-        self._setROParam("fmtstr_map", tmp)
-
-    def doReadFmtstr(self):
-        return self.fmtstr_map[self.opmode]
-
-    def doWriteOpmode(self, value):
-        if self._cache:
-            self._cache.invalidate(self, "value")
-            self._cache.put(self, "fmtstr", self.fmtstr_map[value])
-
-    def _syncOpmode(self, slit_mode, vs_mode):
-        if slit_mode == vs_mode:
-            return
-        self._adevs["slit"]._setROParam("opmode", vs_mode)
-
-
-class VirtualSlit(Moveable):
+class VirtualSource(Moveable):
     """Controller for the Virtual Source Slit System
 
     The slit consists of two L-shaped blades controlled by 5 motors:
@@ -122,9 +21,7 @@ class VirtualSlit(Moveable):
 
     Axis stated are defined via the right-hand rule with +x heading down the beamline towards the sample
 
-    The vertical height of the slit is determined by standard slit motions, however
-    the width is determined by the gap distance between the blades along the x-axis
-    and a shared rotation around the z-axis.
+    Device allows for control and reading of all 5 motor positions.
     """
 
     parameters = {
@@ -141,9 +38,16 @@ class VirtualSlit(Moveable):
             mandatory=False,
             userparam=False,
             default={
-                "4blades": "(%.3f mm x %.3f mm) %.3f deg",
-                "centered": "(%.3f mm x %.3f mm) %.3f deg",
+                "4blades": "%.2f %.2f %.2f %.2f %.2f",
+                "centered": "(%.2f mm x %.2f mm) %.2f deg",
             },
+        ),
+        "offsets": Param(
+            "Change the offset(s) of the virtual source\n"
+            "In order of: left, right, bottom, top, rotation",
+            type=tupleof(float, float, float, float, float),
+            settable=True,
+            default=(0.0, 0.0, 0.0, 0.0, 0.0),
         ),
     }
     parameter_overrides = {
@@ -156,38 +60,29 @@ class VirtualSlit(Moveable):
         "rot": Attach("the rotation stage", Moveable),
     }
 
-    def _findAngle(self, width, gap):
-        angle = np.arcsin(width / (2 * gap))
-        return np.rad2deg(angle)
+    def doInit(self, mode):
+        self.doWriteOffsets(self.offsets)
 
-    def _findGap(self, pos, angle):
+    def doWriteOffsets(self, offset):
+        slitBlades = ["left", "right", "bottom", "top"]
+        self._adevs["rot"]._setROParam("offset", offset[4])
+
+        for blade, blade_offset in zip(slitBlades, offset[:-1]):
+            self._adevs["slit"]._adevs[blade]._setROParam("offset", blade_offset)
+
+    def _returnGap(self, pos):
         # [-left, +right, -bottom, +top]
-        l, r, b, t = pos
-        height = t - b
-
-        rad = np.deg2rad(angle)
-
-        left_gap = float(-(l * np.sin(rad)))
-        right_gap = float(r * np.sin(rad))
-        width = left_gap + right_gap
-
+        left, right, bottom, top = pos
+        width = right - left
+        height = top - bottom
         return [width, height]
 
     def _parseTargets(self, target):
-        # target = [slit width, blade gap, slit height]
+        # angle target must be split from slit target since it is an independent attachment
         if self.opmode == "centered":
-            slit_target = target[1:]
-            angle = self._findAngle(target[0], target[1])
-            return [slit_target, angle]
+            return [target[:-1], target[2]]
         else:
             return [target[:-1], target[4]]
-
-    def _doReadPositions(self, maxage):
-        positions = self._adevs["slit"]._doReadPositions(maxage)
-        angle = self._adevs["rot"].read(maxage)
-
-        width, height = self._findGap(positions, angle)
-        return [width, height, angle]
 
     def doStart(self, target):
         for name, pos in zip(self.devices, self._parseTargets(target)):
@@ -202,7 +97,14 @@ class VirtualSlit(Moveable):
 
     def doRead(self, maxage=0):
         self._syncOpmode(self._adevs["slit"].opmode, self.opmode)
-        return self._doReadPositions(maxage)
+        positions = self._adevs["slit"]._doReadPositions(maxage)
+        angle = self._adevs["rot"].read(maxage)
+
+        if self.opmode == "centered":
+            width, height = self._returnGap(positions)
+            return [width, height, angle]
+        left, right, bottom, top = positions
+        return [left, right, bottom, top, angle]
 
     def doStatus(self, maxage=0):
         return multiStatus(self._adevs, maxage=maxage)
@@ -211,8 +113,8 @@ class VirtualSlit(Moveable):
         if self.opmode == "centered":
             return (
                 Value("Slit Width", unit="mm", fmtstr="%.3f"),
-                Value("Blade Gap", unit="mm", fmtstr="%.3f"),
                 Value("Slit Height", unit="mm", fmtstr="%.3f"),
+                Value("Angle", unit="deg", fmtstr="%.3f"),
             )
         else:
             return (
@@ -245,6 +147,7 @@ class VirtualSlit(Moveable):
             self._cache.put(self, "fmtstr", self.fmtstr_map[value])
 
     def _syncOpmode(self, slit_mode, vs_mode):
+        # the general slit device is locked to the opmode of the virtual source device
         if slit_mode == vs_mode:
             return
         self._adevs["slit"]._setROParam("opmode", vs_mode)
