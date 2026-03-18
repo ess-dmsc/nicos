d38c33a7f1e1af7b8551be7321695d27f3ed01a6
Jonas Petersson <jonas.petersson@ess.eu>
2026-03-18 15:44:56 +0100

    whoops, added that by mistake

--- nicos_ess/ymir/setups/special/collector.py
+++ nicos_ess/ymir/setups/special/collector.py
@@ -2,18 +2,15 @@ description = "setup for the NICOS collector"
 group = "special"
 
 devices = dict(
-    # CacheKafka=device(
-    #     "nicos_ess.devices.cache_kafka_forwarder.CacheKafkaForwarder",
-    #     dev_ignore=["space", "sample"],
-    #     brokers=configdata("config.KAFKA_BROKERS"),
-    #     output_topic="ymir_nicos_devices",
-    # ),
-    CarbonForwarder=device(
-        "nicos_ess.devices.metrics_forwarder.CarbonForwarder",
+    CacheKafka=device(
+        "nicos_ess.devices.cache_kafka_forwarder.CacheKafkaForwarder",
+        dev_ignore=["space", "sample"],
+        brokers=configdata("config.KAFKA_BROKERS"),
+        output_topic="ymir_nicos_devices",
     ),
     Collector=device(
         "nicos.services.collector.Collector",
         cache="localhost:14869",
-        forwarders=["CarbonForwarder"],
+        forwarders=["CacheKafka"],
     ),
 )
