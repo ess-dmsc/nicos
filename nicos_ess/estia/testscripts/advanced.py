03e9d745c7552ed1bfa8b63b0bd8ad42815db81b
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2026-03-19 17:49:21 +0100

    ESTIA: Mover setups need unique device names

--- nicos_ess/estia/testscripts/advanced.py
+++ nicos_ess/estia/testscripts/advanced.py
@@ -1,7 +1,7 @@
 # pylint: skip-file
 
 # test: subdirs = estia
-# test: setups = bandwith_chopper, middle_focus, motion_cabinet_1, motion_cabinet_2, sample_slit, sg1-cart, sg1-mover, sg1-interferometer, sg2-cart, sg2-mover, sg2-interferometer, virtual_source, just-bin-it
+# test: setups = bandwith_chopper, middle_focus, motion_cabinet_1, motion_cabinet_2, sample_slit, sg1-cart, sg1-mover, sg1-interferometer, sg1-screwdriver, sg2-cart, sg2-mover, sg2-interferometer, sg2-screwdriver, virtual_source, just-bin-it
 # test: needs = streaming_data_types
 # test: needs = confluent_kafka
 # test: needs = yuos_query
