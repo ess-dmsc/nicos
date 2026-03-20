b0f97ca367dad799d8c690b471ca7f471ab63a23
Renee Helfert <renee.helfert@ess.eu>
2026-03-20 10:37:54 +0100

    fix duplicate device naming

--- nicos_ess/estia/setups/y_stage.py
+++ nicos_ess/estia/setups/y_stage.py
@@ -2,7 +2,7 @@ description = "Solid Liquid Sample Changer Horizontal Stage"
 
 
 devices = dict(
-    analyzer_lift=device(
+    sample_changer_horizontal=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="SLSC Horizontal Motion",
         motorpv="ESTIA-SpSt:MC-LinY-01:Mtr",
