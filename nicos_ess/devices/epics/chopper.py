d406fb3900326c478ad47ac77b27b276e7342baf
George Nicolas Kontogiorgos <george.kontogiorgos@ess.eu>
2026-03-23 15:00:36 +0100

    chopper alarms: change ref alarm PV name

--- nicos_ess/devices/epics/chopper.py
+++ nicos_ess/devices/epics/chopper.py
@@ -47,7 +47,7 @@ class ChopperAlarms(EpicsParameters, Readable):
         "level": RecordInfo("", "Lvl_Alrm", RecordType.STATUS),
         "position": RecordInfo("", "Pos_Alrms", RecordType.STATUS),
         "power": RecordInfo("", "Pwr_Alrms", RecordType.STATUS),
-        "reference": RecordInfo("", "Ref_Alrms", RecordType.STATUS),
+        "reference": RecordInfo("", "Comm_Ref_Warn", RecordType.STATUS),
         "software": RecordInfo("", "SW_Alrms", RecordType.STATUS),
         "voltage": RecordInfo("", "Volt_Alrms", RecordType.STATUS),
     }
