9e73a76b051cf0b4b7c98911ea6c44c8a73624e8
Jonas Petersson <jonas.petersson@ess.eu>
2026-03-25 15:21:47 +0100

    remove licence/module comments

--- nicos_ess/telemetry/carbon/__init__.py
+++ nicos_ess/telemetry/carbon/__init__.py
@@ -1,35 +1,11 @@
-# *****************************************************************************
-# NICOS, the Networked Instrument Control System of the MLZ
-# Copyright (c) 2009-2024 by the NICOS contributors (see AUTHORS)
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# Module authors:
-#
-#   Jonas Petersson <jonas.petersson@ess.eu>
-#
-# *****************************************************************************
-
 """Carbon/Graphite telemetry backend."""
 
 from nicos_ess.telemetry.carbon.cache_metrics import (
-    CACHE_STATUS_KEY_SUFFIX,
     CACHE_METRIC_KEY_FILTERS,
+    CACHE_STATUS_KEY_SUFFIX,
     CACHE_VALUE_KEY_SUFFIX,
-    CacheMetricsEmitter,
     SCRIPTS_KEY,
+    CacheMetricsEmitter,
 )
 from nicos_ess.telemetry.carbon.client import CarbonTcpClient
 from nicos_ess.telemetry.carbon.config import CarbonConfig
