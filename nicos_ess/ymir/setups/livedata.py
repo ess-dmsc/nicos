da003245ed669995d57bab25f7fea47f8ff2bb8f
linemoller <line.moller@ess.eu>
2026-03-30 13:19:17 +0200

    setup livedata on ymir

---
+++ nicos_ess/ymir/setups/livedata.py
@@ -0,0 +1,18 @@
+description = "ESSlivedata setup"
+
+devices = dict(
+    channel_1=device(
+        "nicos_ess.devices.datasources.livedata.DataChannel",
+        description="A livedata channel",
+        type="counter",
+    ),
+    channel_collector=device(
+        "nicos_ess.devices.datasources.livedata.LiveDataCollector",
+        description="The livedata collector",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        data_topics=["bifrost_livedata_data"],
+        command_topic="bifrost_livedata_commands",
+        status_topics=["bifrost_livedata_status"],
+        others=["channel_1"],
+    ),
+)
