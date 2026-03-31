9fdccee137a6219837d78a91eaf5188b31b437e2
linemoller <line.moller@ess.eu>
2026-03-31 14:55:01 +0200

    feat(loki): prepare for do_sans and do_trans commands

--- nicos_ess/loki/commands/scripting.py
+++ nicos_ess/loki/commands/scripting.py
@@ -1,21 +1,18 @@
 from nicos.commands import usercommand
 
-
-@usercommand
-def do_trans(trans_duration, trans_duration_type):
-    pass
+__all__ = ["do_trans", "do_sans", "do_simultaneous"]
 
 
 @usercommand
-def do_sans(sans_duration, sans_duration_type):
-    pass
+def do_trans(trans_duration=None, trans_duration_type=None):
+    raise NotImplementedError("Please implement do_trans script")
 
 
 @usercommand
-def do_simultaneous(sans_duration, sans_duration_type):
-    pass
+def do_sans(sans_duration=None, sans_duration_type=None):
+    raise NotImplementedError("Please implement do_sans script")
 
 
 @usercommand
-def set_sample(name, thickness):
-    pass
+def do_simultaneous(sans_duration=None, sans_duration_type=None):
+    raise NotImplementedError("Please implement do_simultanous script")
