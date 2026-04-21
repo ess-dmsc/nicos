7c79930a6403cdd07c28f70d1a13ec9c6bba527f
linemoller <line.moller@ess.eu>
2026-04-21 14:28:53 +0200

    add pump2

--- nicos_ess/nido/setups/cetoni_minimal.py
+++ nicos_ess/nido/setups/cetoni_minimal.py
@@ -1,17 +1,27 @@
 description = "The cetoni pumps"
 
 pump1_pvroot = "B02-CSLab:SE-Pumps:SP1"
+pump2_pvroot = "B02-CSLab:SE-Pumps:SP2"
 
 devices = dict(
     pump1=device(
         "nicos_ess.loki.devices.cetoni_minimal.CetoniPumpController",
         description="Description",
         pvroot=pump1_pvroot,
-        # syringe_config="pump1_syringe_config",
     ),
     pump1_syringe_config=device(
         "nicos_ess.devices.epics.pva.EpicsMappedMoveable",
         readpv=f"{pump1_pvroot}SyrType-SP",
         writepv=f"{pump1_pvroot}SyrType-SP",
+    ),
+    pump2=device(
+        "nicos_ess.loki.devices.cetoni_minimal.CetoniPumpController",
+        description="Description",
+        pvroot=pump2_pvroot,
+    ),
+    pump2_syringe_config=device(
+        "nicos_ess.devices.epics.pva.EpicsMappedMoveable",
+        readpv=f"{pump2_pvroot}SyrType-SP",
+        writepv=f"{pump2_pvroot}SyrType-SP",
     ),
 )
