0acc898b7ce3431a84a97fbdc5e9e0312d669a18
Hanno Perrey <hanno.perrey@ess.eu>
2026-04-24 11:36:26 +0200

    fix(setups): spelling of `visibility` param

--- nicos_ess/loki/setups/vacuum.py
+++ nicos_ess/loki/setups/vacuum.py
@@ -6,24 +6,24 @@ devices = dict(
         description="Vacuum gauge - MicroPirani sensor pressure (PR1), MKS910 IOC",
         readpv="LOKI-VacInstr:Vac-VGF-050:PR3-R",
         fmtstr="%.6f",
-        visiblitity=(),
+        visibility=(),
     ),
     sample_area_vacuum_gate_valve_interlock=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Vacuum gauge - the status of the gate valve interlock",
         readpv="LOKI-VacInstr:Vac-VVS-400:IntlckLED",
-        visiblitity=(),
+        visibility=(),
     ),
     sample_area_vacuum_valve_open=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Vacuum valve - PLC",
         readpv="LOKI-VacInstr:Vac-VVS-400:OpenR",
-        visiblitity=(),
+        visibility=(),
     ),
     sample_area_vacuum_valve_closed=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Vacuum valve - PLC",
         readpv="LOKI-VacInstr:Vac-VVS-400:ClosedR",
-        visiblitity=(),
+        visibility=(),
     ),
 )
