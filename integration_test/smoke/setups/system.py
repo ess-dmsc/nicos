5a063c97a9931a3922502beb64a4c1e9a7ac479f
Jonas Petersson <jonas.petersson@ess.eu>
2026-04-24 16:39:27 +0200

    try packaging the integration test instrument code

--- integration_test/smoke/setups/system.py
+++ integration_test/smoke/setups/system.py
@@ -6,6 +6,7 @@ description = "system setup for NICOS smoke integration"
 group = "lowlevel"
 
 _runtime_root = os.environ["NICOS_SMOKE_RUNTIME_ROOT"]
+_nexus_config_path = os.environ["NICOS_SMOKE_NEXUS_CONFIG_PATH"]
 
 sysconfig = dict(
     cache=configdata("config.CACHE_HOST"),
@@ -53,7 +54,7 @@ devices = dict(
     NexusStructure_Basic=device(
         "nicos_ess.devices.datasinks.nexus_structure.NexusStructureJsonFile",
         description="Provides the NeXus structure",
-        nexus_config_path="integration_test/smoke/nexus/smoke_nexus.json",
+        nexus_config_path=_nexus_config_path,
         instrument_name="",
         visibility=(),
     ),
