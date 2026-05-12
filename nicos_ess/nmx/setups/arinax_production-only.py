b2c5c2bcd0547a7813815c37a8a98af9d5163581
Lais Pessine do Carmo <lais.pessine@ess.eu>
2026-05-12 14:45:48 +0200

    Re-add production-only file
    It was lost after last cherry-picking.

---
+++ nicos_ess/nmx/setups/arinax_production-only.py
@@ -0,0 +1,48 @@
+description = "ARINAX controls - The PVs here are available only in production! To be tested."
+
+group = "optional"
+
+devices = dict(
+    # Backlight
+    backlight_level=device(
+        "nicos_ess.devices.epics.pva.EpicsDigitalMoveable",
+        description="ARINAX Backlight Level",
+        readpv="NMX:BackLightLevel",
+        writepv="NMX:BackLightLevel",
+        pva=False,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    ),
+    backlight_position=device(
+        "nicos_ess.devices.epics.pva.EpicsDigitalMoveable",
+        description="ARINAX Backlight Position",
+        readpv="NMX:BackLightPOS",
+        writepv="NMX:BackLightPOS",
+        pva=False,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    ),
+    # Zoom
+    zoom_level=device(
+        "nicos_ess.devices.epics.pva.EpicsDigitalMoveable",
+        description="ARINAX Zoom Level",
+        readpv="NMX:ZoomLevel",
+        writepv="NMX:ZoomLevel",
+        pva=False,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    ),
+    zoom_range=device(
+        "nicos_ess.devices.epics.pva.EpicsReadable",
+        description="ARINAX Zoom Range",
+        readpv="NMX:getZoomRange",
+        writepv="NMX:getZoomRange",
+        pva=False,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    ),
+)
