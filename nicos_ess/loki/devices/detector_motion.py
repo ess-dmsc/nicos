af6c57ae0102a1ec4d33ccfbc244dfb4f082e97e
linemoller <line.moller@ess.eu>
2026-05-12 13:51:06 +0200

    fix(loki): add power supply as attached device

--- nicos_ess/loki/devices/detector_motion.py
+++ nicos_ess/loki/devices/detector_motion.py
@@ -1,8 +1,9 @@
-from nicos import session
 from nicos.core import (
+    Attach,
     Param,
     status,
 )
+from nicos_ess.devices.epics.power_supply_channel import PowerSupplyBank
 from nicos_ess.devices.epics.pva.motor import EpicsMotor
 
 
@@ -13,11 +14,6 @@ class LOKIDetectorMotion(EpicsMotor):
     """
 
     parameters = {
-        "ps_bank_name": Param(
-            "Detector bank power supply name in setup",
-            type=str,
-            mandatory=True,
-        ),
         "voltage_off_threshold": Param(
             "The voltage threshold for when the power supply channel is considered off",
             type=float,
@@ -25,19 +21,16 @@ class LOKIDetectorMotion(EpicsMotor):
         ),
     }
 
+    attached_devices = {
+        "power_supply": Attach("Power supply for detector bank 0", PowerSupplyBank),
+    }
+
     def doInit(self, mode):
         EpicsMotor.doInit(self, mode)
-        self._ps_bank = self._get_ps_bank()
-
-    def _get_ps_bank(self):
-        ps_bank = session.devices[self.ps_bank_name]
-        if not ps_bank:
-            self.log.error("Power supply instance does not exist")
-        return ps_bank
 
     def _bank_status_is_ok(self):
         # TODO: Move check to power supply class
-        bank_status, status_msg = self._ps_bank.status()
+        bank_status, status_msg = self._attached_power_supply.status()
         if bank_status != status.OK:
             self.log.warning(status_msg)
             return False
@@ -45,7 +38,7 @@ class LOKIDetectorMotion(EpicsMotor):
 
     def _bank_is_powered_off(self):
         # TODO: Move check to power supply class
-        bank_on, n_channels_on = self._ps_bank.status_on()
+        bank_on, n_channels_on = self._attached_power_supply.status_on()
         if bank_on:
             self.log.warning("Power supply bank is still ON, all channels must be OFF.")
             return False
@@ -53,12 +46,13 @@ class LOKIDetectorMotion(EpicsMotor):
 
     def _bank_voltage_is_below_threshold(self):
         # TODO: Move check to power supply class
-        for channel in self._ps_bank._attached_ps_channels:
+        for channel in self._attached_power_supply._attached_ps_channels:
             if channel.doReadVoltage_Monitor() > self.voltage_off_threshold:
                 self.log.warning(
                     "Power supply bank voltages are above threshold, "
                     "all channels must be less than "
-                    f"{self.voltage_off_threshold} {self._ps_bank._get_voltage_unit()}"
+                    f"{self.voltage_off_threshold} "
+                    f"{self._attached_power_supply._get_voltage_unit()}"
                 )
                 return False
         return True
@@ -87,7 +81,8 @@ class LOKIDetectorMotion(EpicsMotor):
             and self._bank_voltage_is_below_threshold()
         ):
             self.log.info(
-                "Detector bank motion: Power supply is OFF and voltage is below or equal to threshold. "
+                "Detector bank motion: Power supply is OFF and "
+                "voltage is below or equal to threshold. "
                 "Movement is permitted."
             )
             return (
--- test/setups/custom/ess_loki_detector_carriage.py
+++ test/setups/custom/ess_loki_detector_carriage.py
@@ -4,7 +4,7 @@ devices = dict(
     restricted_motor=device(
         "test.nicos_ess.loki.test_loki_detector_carriage.FakeLokiDetectorMotion",
         motorpv="IOC:m1",
-        ps_bank_name="ps_bank_hv",
+        power_supply="ps_bank_hv",
         voltage_off_threshold=5.0,
         has_powerauto=False,
         has_msgtxt=False,
