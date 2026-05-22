62fca61ae5540aab0209569ba4fec9d61e320a93
linemoller <line.moller@ess.eu>
2026-05-22 10:36:14 +0200

    initial integration

---
+++ nicos_ess/nido/setups/cetoni_pumps.py
@@ -0,0 +1,32 @@
+description = "The cetoni pumps"
+
+pump1_pvroot = "B02-CSLab:SE-Pumps:SP1"
+pump2_pvroot = "B02-CSLab:SE-Pumps:SP2"
+linked_pvroot = "B02-CSLab:SE-Pumps:Lnk"
+
+devices = dict(
+    pump1=device(
+        "nicos_ess.loki.devices.cetoni_pump.CetoniPumpController",
+        description="Description",
+        pvroot=pump1_pvroot,
+    ),
+    pump1_syringe_config=device(
+        "nicos_ess.devices.epics.pva.EpicsMappedMoveable",
+        readpv=f"{pump1_pvroot}SyrType-SP",
+        writepv=f"{pump1_pvroot}SyrType-SP",
+    ),
+    pump2=device(
+        "nicos_ess.loki.devices.cetoni_pump.CetoniPumpController",
+        description="Description",
+        pvroot=pump2_pvroot,
+    ),
+    pump2_syringe_config=device(
+        "nicos_ess.devices.epics.pva.EpicsMappedMoveable",
+        readpv=f"{pump2_pvroot}SyrType-SP",
+        writepv=f"{pump2_pvroot}SyrType-SP",
+    ),
+    # pump_linked_mode=device(
+    #     "nicos_ess.loki.devices.cetoni_minimal.CetoniPumpLinkedMode",
+    #     pvroot="",
+    # ),
+)
