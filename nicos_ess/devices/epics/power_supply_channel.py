71cd365266c8f6e7d2531382e0672606ff244205
linemoller <line.moller@ess.eu>
2026-05-28 09:43:48 +0200

    limit display of current and voltage to 3 decimal points

--- nicos_ess/devices/epics/power_supply_channel.py
+++ nicos_ess/devices/epics/power_supply_channel.py
@@ -44,6 +44,7 @@ class PowerSupplyChannel(EpicsParameters, CanDisable, MappedReadable):
             # Setting them anyway just for readability.
             internal=True,
             userparam=True,
+            fmtstr="%.3f",
         ),
         "voltage_units": Param(
             "Voltage monitor readback units",
@@ -55,6 +56,7 @@ class PowerSupplyChannel(EpicsParameters, CanDisable, MappedReadable):
             volatile=True,
             internal=True,
             userparam=True,
+            fmtstr="%.3f",
         ),
         "current_units": Param(
             "Current monitor readback units",
