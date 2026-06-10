4a2bb224668264bf603809ace9dfe1f7e073e636
linemoller <line.moller@ess.eu>
2026-06-10 13:01:47 +0200

    add homing warning dialog

---
+++ nicos_ess/gui/dialogs/homing_check.py
@@ -0,0 +1,17 @@
+from nicos.guisupport.qt import QDialog, QDialogButtonBox, QLabel, QVBoxLayout
+
+
+class HomingCheckDialog(QDialog):
+    def __init__(self, message="Are you sure?"):
+        QDialog.__init__(self)
+        self.message = message
+
+        buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        button_box = QDialogButtonBox(buttons)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+        layout = QVBoxLayout()
+        message = QLabel(self.message)
+        layout.addWidget(message)
+        layout.addWidget(button_box)
+        self.setLayout(layout)
