ba6243851d0f5bce48ae3d14851808c047371339
linemoller <line.moller@ess.eu>
2026-06-15 13:53:31 +0200

    update nexus path in test

--- test/nicos_ess/test_devices/test_dynamic_nexus_building.py
+++ test/nicos_ess/test_devices/test_dynamic_nexus_building.py
@@ -317,7 +317,7 @@ class TestDynamicNexusBuilding(TestCase):
         assert sample_name_ds["config"]["values"] == "SampleE"
 
         cellholder_ds = get_by_named_path(
-            doc, path_map, "/entry/instrument/sample_changer"
+            doc, path_map, "/entry/sample/sample_changer"
         )
         assert cellholder_ds is not None
         assert cellholder_ds["children"][0]["config"]["values"] == "T5"
