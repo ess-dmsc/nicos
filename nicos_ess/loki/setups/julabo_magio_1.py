d91b69bfc2eea2210dee767c8d7dce21767d9a07
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2026-06-29 14:22:09 +0200

    LOKI: Update StatusMsg-R to new Status-R PV

--- nicos_ess/loki/setups/julabo_magio_1.py
+++ nicos_ess/loki/setups/julabo_magio_1.py
@@ -89,8 +89,7 @@ devices = dict(
     julabo_1_status_message=device(
         "nicos_ess.devices.epics.pva.EpicsReadable",
         description="The status message",
-        readpv="{}StatusMsg-R".format(pv_root),
-        visibility=(),
+        readpv="{}Status-R".format(pv_root),
     ),
     julabo_1_version=device(
         "nicos_ess.devices.epics.pva.EpicsStringReadable",
