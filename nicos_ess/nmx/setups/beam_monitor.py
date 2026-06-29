785ab65172d5c4650611f6edf49c14452ca30cea
Lais Pessine do Carmo <lais.pessine@ess.eu>
2026-06-29 14:57:01 +0200

    Reorder, rename and visibility of beam monitor actuator

--- nicos_ess/nmx/setups/beam_monitor.py
+++ nicos_ess/nmx/setups/beam_monitor.py
@@ -4,7 +4,15 @@ pv_root = "NMX-BM2:MC-Pne-01:"
 
 # TODO: Use mapped or epics shutter?  
 devices = dict(
-    beam_monitor_status=device(
+    beam_monitor=device(
+        "nicos_ess.devices.epics.pva.shutter.EpicsShutter",
+        description="Beam monitor actuator",
+        writepv=f"{pv_root}ShtOpen",
+        readpv=f"{pv_root}ShtAuxBits07",
+        resetpv=f"{pv_root}ShtErrRst",
+        msgtxt=f"{pv_root}ShtMsgTxt",
+    ),
+    beam_monitor__status=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="Beam monitor actuator status",
         readpv=f"{pv_root}ShtAuxBits07",
@@ -12,8 +20,9 @@ devices = dict(
         monitor=True,
         pollinterval=0.5,
         maxage=None,
+        visibility={},
     ),
-    beam_monitor=device(
+    beam_monitor__control=device(
         "nicos_ess.devices.epics.pva.EpicsMappedMoveable",
         description="Beam monitor actuator control",
         readpv=f"{pv_root}ShtOpen",
@@ -22,14 +31,6 @@ devices = dict(
         monitor=True,
         pollinterval=0.5,
         maxage=None,
-    ),
-
-    beam_monitor__epics_shutter=device(
-        "nicos_ess.devices.epics.pva.shutter.EpicsShutter",
-        description="Beam monitor actuator",
-        writepv=f"{pv_root}ShtOpen",
-        readpv=f"{pv_root}ShtAuxBits07",
-        resetpv=f"{pv_root}ShtErrRst",
-        msgtxt=f"{pv_root}ShtMsgTxt",
+        visibility={},
     ),
 )
