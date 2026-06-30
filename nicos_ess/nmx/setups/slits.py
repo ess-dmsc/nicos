6fa5446f20736250c112bd5b3d0a7f58b5bbe73c
Lais Pessine do Carmo <lais.pessine@ess.eu>
2026-06-30 14:19:39 +0200

    Clean up cabinet info from slits

--- nicos_ess/nmx/setups/slits.py
+++ nicos_ess/nmx/setups/slits.py
@@ -4,34 +4,32 @@ pv_root = "NMX"
 slit_set_1_pv_root = f"{pv_root}-ColSl1:"
 slit_set_2_pv_root = f"{pv_root}-ColSl2:"
 
-# TODO: Check descriptions (axis and cabinet numbers)!
-
 devices = dict(
     # Slit 1 blades
     slit_set_1_left_blade=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimation slit set 1 left blade - electrical axis 1 in motion cabinet 2",
+        description="Collimation slit set 1 left blade",
         motorpv=f"{slit_set_1_pv_root}MC-SlYp-01:Mtr",
         monitor_deadband=0.01,
         visibility={},
     ),
     slit_set_1_right_blade=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimation slit set 1 right blade - electrical axis 2 in motion cabinet 2",
+        description="Collimation slit set 1 right blade",
         motorpv=f"{slit_set_1_pv_root}MC-SlYm-01:Mtr",
         monitor_deadband=0.01,
         visibility={},
     ),
     slit_set_1_upper_blade=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimation slit set 1 upper blade - electrical axis 3 in motion cabinet 2",
+        description="Collimation slit set 1 upper blade",
         motorpv=f"{slit_set_1_pv_root}MC-SlZp-01:Mtr",
         monitor_deadband=0.01,
         visibility={},
     ),
     slit_set_1_lower_blade=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimation slit set 1 lower blade - electrical axis 4 in motion cabinet 2",
+        description="Collimation slit set 1 lower blade",
         motorpv=f"{slit_set_1_pv_root}MC-SlZm-01:Mtr",
         monitor_deadband=0.01,
         visibility={},
@@ -64,28 +62,28 @@ devices = dict(
     # Slit 2 blades
     slit_set_2_left_blade=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimation slit set 2 left blade - electrical axis 5 in motion cabinet 2",
+        description="Collimation slit set 2 left blade",
         motorpv=f"{slit_set_2_pv_root}MC-SlYp-01:Mtr",
         monitor_deadband=0.01,
         visibility={},
     ),
     slit_set_2_right_blade=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimation slit set 2 right blade - electrical axis 6 in motion cabinet 2",
+        description="Collimation slit set 2 right blade",
         motorpv=f"{slit_set_2_pv_root}MC-SlYm-01:Mtr",
         monitor_deadband=0.01,
         visibility={},
     ),
     slit_set_2_upper_blade=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimation slit set 2 upper blade - electrical axis 7 in motion cabinet 2",
+        description="Collimation slit set 2 upper blade",
         motorpv=f"{slit_set_2_pv_root}MC-SlZp-01:Mtr",
         monitor_deadband=0.01,
         visibility={},
     ),
     slit_set_2_lower_blade=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
-        description="Collimation slit set 2 lower blade - electrical axis 8 in motion cabinet 2",
+        description="Collimation slit set 2 lower blade",
         motorpv=f"{slit_set_2_pv_root}MC-SlZm-01:Mtr",
         monitor_deadband=0.01,
         visibility={},
