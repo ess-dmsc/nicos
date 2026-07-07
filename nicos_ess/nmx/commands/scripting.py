96dbbd144a273235c4c9f37ca169d35e3d0c2203
Lais Pessine do Carmo <lais.pessine@ess.eu>
2026-07-07 15:59:50 +0200

    Clean up script

--- nicos_ess/nmx/commands/scripting.py
+++ nicos_ess/nmx/commands/scripting.py
@@ -13,23 +13,8 @@ PINHOLE_DEV_NAMES = [
 
 
 @usercommand
-def do_trans(trans_duration=None, trans_duration_type=None):
-    raise NotImplementedError("Please implement do_trans script")
-
-
-@usercommand
-def do_sans(sans_duration=None, sans_duration_type=None):
-    raise NotImplementedError("Please implement do_sans script")
-
-
-@usercommand
-def do_simultaneous(sans_duration=None, sans_duration_type=None):
-    raise NotImplementedError("Please implement do_simultanous script")
-
-
-@usercommand
 def nmx_pinhole_reset():
-    """ Reset all pinhole motion devices in a single command."""
+    """ Reset all NMX pinhole motion devices in a single command."""
     for device_name in PINHOLE_DEV_NAMES:
         try:
             dev = session.getDevice(device_name)
