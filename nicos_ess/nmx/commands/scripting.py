19bb13ba19e3001563ce3c79d06d773db81b07e5
Lais Pessine do Carmo <lais.pessine@ess.eu>
2026-07-07 15:54:29 +0200

    Add nmx pinhole reset user command

--- nicos_ess/nmx/commands/scripting.py
+++ nicos_ess/nmx/commands/scripting.py
@@ -1,6 +1,15 @@
 from nicos.commands import usercommand
+from nicos import session
 
 __all__ = ["do_trans", "do_sans", "do_simultaneous", "nmx_pinhole_reset"]
+
+PINHOLE_DEV_NAMES = [
+    "pinhole__virtual_motor",
+    "pinhole__carousel_rotation",
+    "pinhole__carousel_electromagnet",
+    "pinhole__carousel_v_raise",
+    "pinhole__aperture_arm"
+]
 
 
 @usercommand
@@ -19,6 +28,16 @@ def do_simultaneous(sans_duration=None, sans_duration_type=None):
 
 
 @usercommand
-def nmx_pinhole_reset(pin_duration=None, pin_type=None):
-    raise NotImplementedError("Please implement nmx_pinhole_reset script")
+def nmx_pinhole_reset():
+    """ Reset all pinhole motion devices in a single command."""
+    for device_name in PINHOLE_DEV_NAMES:
+        try:
+            dev = session.getDevice(device_name)
+        except Exception as e:
+            print("ERROR: " + str(e))
+            continue
+        else:
+            dev.reset()
+            print(f"{device_name} was reset.")
+    print("Finished pinhole reset!")
 
