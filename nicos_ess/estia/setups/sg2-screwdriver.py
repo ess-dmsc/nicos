d570bc162e9e9db5e2115d0d5a7acb9026f02260
Renee Helfert <renee.helfert@ess.eu>
2026-07-13 10:13:46 +0200

    add homing notification on some selene guide motors

--- nicos_ess/estia/setups/sg2-screwdriver.py
+++ nicos_ess/estia/setups/sg2-screwdriver.py
@@ -6,11 +6,13 @@ devices = dict(
     sg2_robot_pos=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Robot horizontal positioning",
+        home_warning_msg="Are you sure you want to home the motor?",
         motorpv=f"{pvprefix}LinX-01:Mtr",
     ),
     sg2_robot_vert=device(
         "nicos_ess.devices.epics.pva.motor.EpicsMotor",
         description="Robot vertical positioning",
+        home_warning_msg="Are you sure you want to home the motor?",
         motorpv=f"{pvprefix}LinZ-01:Mtr",
     ),
     sg2_screwdriver_adjust_1=device(
