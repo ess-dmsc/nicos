2b7a1d32b6a554db23ffdc9bc3706cdefa4f4911
matt <matt.clarke@ess.eu>
2026-07-13 11:13:47 +0200

    formatting

--- nicos_ess/gui/dialogs/motor.py
+++ nicos_ess/gui/dialogs/motor.py
@@ -245,7 +245,9 @@ class MotorDialog(QDialog):
 
     @pyqtSlot()
     def on_btn_set_offset_clicked(self):
-        val = self._get_new_value("Adjust offset", "Redefine current position of %s" % self.devname)
+        val = self._get_new_value(
+            "Adjust offset", "Redefine current position of %s" % self.devname
+        )
         if val is not None:
             self.devices_panel.exec_command("adjust(%s, %r)" % (self.devrepr, val))
 
