fd45f3f769d3b01ddc5be45bdd17530c36f2e3cb
Hanno Perrey <hanno.perrey@ess.eu>
2026-08-10 09:00:04 +0200

    fix(numpy2): `ruff` unsafe fixes
    `ruff check . --fix --unsafe-fixes --select NPY201`

--- nicos_ess/gui/widgets/pyqtgraph/utils/fitter.py
+++ nicos_ess/gui/widgets/pyqtgraph/utils/fitter.py
@@ -2,6 +2,7 @@ import warnings
 from enum import Enum
 
 import numpy as np
+from numpy.exceptions import RankWarning
 from scipy.optimize import curve_fit
 
 from nicos.guisupport.qt import QObject
@@ -108,7 +109,7 @@ class Fitter1D(QObject):
             warnings.filterwarnings("error")
             try:
                 p = np.polyfit(self.x_data, self.y_data, 2)
-            except np.RankWarning:
+            except RankWarning:
                 print("Data not suitable for quadratic fit")
                 return None
 
