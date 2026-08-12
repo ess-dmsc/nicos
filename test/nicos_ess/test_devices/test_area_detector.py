11237c0f21d29abbf66b88415d1e3d2fbfdccc9f
Renee Helfert <renee.helfert@ess.eu>
2026-08-12 10:43:17 +0200

    update FakeAreaDetector for tests

--- test/nicos_ess/test_devices/test_area_detector.py
+++ test/nicos_ess/test_devices/test_area_detector.py
@@ -1,38 +1,36 @@
-#!/usr/bin/env python3
-from unittest.mock import patch
 import pytest
 import numpy as np
 from nicos.core.params import ArrayDesc
 from nicos_ess.devices.epics.area_detector import AreaDetector
+from test.nicos_ess.test_devices.doubles.epics_pva_backend import FakeEpicsComponent
 
 session_setup = None
 
-
 class FakeAreaDetector(AreaDetector):
     """Area detector faking a connection."""
-
-    fake_values = {
-        "detector_state.STAT": 0,
-        "detector_state.SEVR": 0,
-        "max_size_x": 1024,
-        "max_size_y": 2048,
-        "data_type": np.uint32,
-        "readpv": 0,
-        "detector_state": 0,
-        "array_rate_rbv": "ArrayRate_RBV",
-        "acquire": "Acquire",
-        "acquire_status": "AcquireBusy",
-    }
+    @classmethod
+    def _initial_channel_values(cls):
+        return {
+            "detector_state.STAT": 0,
+            "detector_state.SEVR": 0,
+            "max_size_x": 1024,
+            "max_size_y": 2048,
+            "data_type": np.uint32,
+            "readpv": 0,
+            "detector_state": 0,
+            "array_rate_rbv": "ArrayRate_RBV",
+            "acquire": "Acquire",
+            "acquire_status": "AcquireBusy",
+        }
 
     def doPreinit(self, mode):
-        with patch("nicos_ess.devices.epics.area_detector.EpicsDevice"):
-            super().doPreinit(mode)
+        self._epics = FakeEpicsComponent(self._initial_channel_values())
 
-    def _put_pv(self, pvparam, value, wait=False):
-        self.fake_values[pvparam] = value
+    def doInit(self, mode):
+        pass
 
-    def _get_pv(self, pvparam, as_string=False):
-        return self.fake_values[pvparam]
+    def _read_channel_cached(self, channel, as_string=None, maxage=None):
+        return self._epics.values[channel]
 
 
 class TestAreaDetector:
@@ -52,9 +50,9 @@ class TestAreaDetector:
 
     def test_completed(self):
         """Test that doIsCompleted() returns correct value."""
-        self.area_detector.fake_values["acquire_status"] = "Done"
+        self.area_detector._epics.values["acquire_status"] = "Done"
         ret = self.area_detector.isCompleted()
         assert ret == True
-        self.area_detector.fake_values["acquire_status"] = "Busybusybusy"
+        self.area_detector._epics.values["acquire_status"] = "Busybusybusy"
         ret = self.area_detector.isCompleted()
         assert ret == False
