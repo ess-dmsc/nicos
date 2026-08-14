338727d4155540be4fb820510d015f7b77fe270a
matt <matt.clarke@ess.eu>
2026-08-14 12:20:22 +0200

    started moving do_sans etc. away from usercommands

--- nicos_ess/commands/script_importer.py
+++ nicos_ess/commands/script_importer.py
@@ -1,18 +1,18 @@
 from nicos.commands import usercommand
 from nicos import session
 import inspect
-import importlib
-import sys
+import os
 
 
 __all__ = [
     "import_instrument_scripts",
 ]
 
+FILENAME = "commands.py"
+
 
 def _import_instrument_scripts(instrument):
-    # Add instrument scripts directory to import path.
-    sys.path.insert(1, session.experiment.instrument_scripts_filepath)
+    # NOTE: currently we can only import one file.
 
     # Find the frame we want to add the commands to.
     # This will be the same frame we find the NICOS commands in.
@@ -21,14 +21,11 @@ def _import_instrument_scripts(instrument):
         if "maw" in i[0].f_globals:
             globs = i[0].f_globals
 
-    # Import the module so we can get the correct path
-    inst_mod = importlib.import_module(instrument)
-    file_loc = inst_mod.__file__[:-1] if inst_mod.__file__.endswith(".pyc") else inst_mod.__file__
-    print(f"File loc {file_loc}")
-
     # Hackery to import the instrument commands into the same namespace as
     # the NICOS commands.
+    file_loc = os.path.join(session.experiment.instrument_scripts_filepath, instrument, FILENAME)
     exec(compile(open(file_loc).read(), file_loc, "exec"), globs)
+    print(f"File loc {file_loc}")
 
 
 @usercommand
