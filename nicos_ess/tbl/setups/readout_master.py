3d0952b03d46a2c775ac3dfb4d34a381dfedf40e
Hanno Perrey <hanno.perrey@ess.eu>
2026-08-14 07:48:26 +0200

    chore(tbl/setups)!: RMM2 PV update for new API
    "WP12 just released a new software version (API + EPICS layer) alongside
    the new RMM firmware. Because of this major release, we renamed some PVs
    to align with ICS naming rules. Could someone share the list of PVs
    currently configured in NICOS? I’ll map them to the new names so we can
    update them on the NICOS side.Note: The new IOC is currently active only
    on TBL - RMM 2 (multi-blade)."

--- nicos_ess/tbl/setups/readout_master.py
+++ nicos_ess/tbl/setups/readout_master.py
@@ -47,41 +47,41 @@ devices = dict(
     rmm2_temperature=device(
         "nicos_ess.devices.epics.pva.EpicsReadable",
         description="The temperature of the hottest FPGA temperature sensor",
-        readpv="{}Temperature-R".format(pv_root_2),
+        readpv="{}MaxTemperature".format(pv_root_2),
     ),
     rmm2_ring_status=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="The status of the ring",
-        readpv="{}RingStatus-R".format(pv_root_2),
+        readpv="{}RingStatus".format(pv_root_2),
     ),
     rmm2_ring_bring_up_output=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="The ring bring up output",
-        readpv="{}RingBringUpOutput-R".format(pv_root_2),
+        readpv="{}RingBringUpOutput".format(pv_root_2),
     ),
     rmm2_config_message=device(
         "nicos_ess.devices.epics.pva.EpicsStringReadable",
         description="The configuration message",
-        readpv="{}ConfigMessage-R".format(pv_root_2),
+        readpv="{}ConfigMessage".format(pv_root_2),
     ),
     rmm2_ref_clock_freq_ok=device(
         "nicos_ess.devices.epics.pva.EpicsMappedReadable",
         description="The reference clock frequency status",
-        readpv="{}RefClkFreqOk-R".format(pv_root_2),
+        readpv="{}RefClkFreqOk".format(pv_root_2),
     ),
     rmm2_mrf_message=device(
         "nicos_ess.devices.epics.pva.EpicsStringReadable",
         description="The MRF message",
-        readpv="{}MrfMsg-R".format(pv_root_2),
+        readpv="{}MrfMsg".format(pv_root_2),
     ),
     rmm2_timing_mode_source=device(
         "nicos_ess.devices.epics.pva.EpicsStringReadable",
         description="The source timing mode",
-        readpv="{}TimingModeSrc-R".format(pv_root_2),
+        readpv="{}TimingModeSrc".format(pv_root_2),
     ),
     rmm2_timing_mode_sync=device(
         "nicos_ess.devices.epics.pva.EpicsStringReadable",
         description="The sync timing mode",
-        readpv="{}TimingModeSync-R".format(pv_root_2),
+        readpv="{}TimingModeSync".format(pv_root_2),
     ),
 )
