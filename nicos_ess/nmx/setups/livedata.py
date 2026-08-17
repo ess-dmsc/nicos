2f4536faea71df49f82753a2b485688bef183954
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2026-08-17 13:43:21 +0200

    Add livedata setup files for the instruments we tested

---
+++ nicos_ess/nmx/setups/livedata.py
@@ -0,0 +1,25 @@
+description = "The nmx livedata."
+
+devices = dict(
+    cbm1_counts_total=device(
+        "nicos_ess.devices.datasources.livedata.DataChannel",
+        description="An nmx livedata channel",
+        device_name="monitor1_counts_total",
+        type="counter",
+    ),
+    cbm2_counts_total=device(
+        "nicos_ess.devices.datasources.livedata.DataChannel",
+        description="An nmx livedata channel",
+        device_name="monitor2_counts_total",
+        type="counter",
+    ),
+    livedata_collector=device(
+        "nicos_ess.devices.datasources.livedata.LiveDataCollector",
+        description="The nmx livedata collector",
+        brokers=configdata("config.KAFKA_BROKERS"),
+        data_topics=["nmx_livedata_nicos_data"],
+        commands_topic="nmx_livedata_commands",
+        status_topics=["nmx_livedata_heartbeat"],
+        others=["cbm1_counts_total", "cbm2_counts_total"],
+    ),
+)
