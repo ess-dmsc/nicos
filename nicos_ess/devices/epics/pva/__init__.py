03b86f85c4b98d61b1aeabf9b555129e96648cad
Dimitrios Arnellos <dimitrios.arnellos@ess.eu>
2026-08-21 11:21:14 +0200

    Revert "Add EpicsManualMappedMoveable device"
    This reverts commit f1edc32377cec630d325bebac8b26771f4216ba3.

--- nicos_ess/devices/epics/pva/__init__.py
+++ nicos_ess/devices/epics/pva/__init__.py
@@ -4,7 +4,6 @@ from nicos_ess.devices.epics.pva.epics_devices import (
     EpicsAnalogMoveable,
     EpicsDigitalMoveable,
     EpicsManualMappedAnalogMoveable,
-    EpicsManualMappedMoveable,
     EpicsMappedMoveable,
     EpicsMappedReadable,
     EpicsNumericReadable,
