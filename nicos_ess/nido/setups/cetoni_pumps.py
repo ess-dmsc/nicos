437c583358ced11fc29d95c6721764189527e072
linemoller <line.moller@ess.eu>
2026-09-08 09:17:49 +0200

    config(loki): setup file for cetoni pumps

---
+++ nicos_ess/nido/setups/cetoni_pumps.py
@@ -0,0 +1,26 @@
+description = "The cetoni pumps"
+
+pump1_pvroot = "B02-CSLab:SE-Pumps:SP1"
+pump2_pvroot = "B02-CSLab:SE-Pumps:SP2"
+linked_pvroot = "B02-CSLab:SE-Pumps:Lnkd"
+
+devices = dict(
+    pump1=device(
+        "nicos_ess.loki.devices.cetoni_pump.CetoniPumpController",
+        description="Control device for cetoni pump SP1",
+        pvroot=pump1_pvroot,
+        readpv=f"{pump1_pvroot}FilledVolume",
+        writepv=f"{pump1_pvroot}FillVol-SP",
+        precision=0.005,
+        home_warning_msg="Please make sure syringes are removed before homing",
+    ),
+    pump2=device(
+        "nicos_ess.loki.devices.cetoni_pump.CetoniPumpController",
+        description="Control device for cetoni pump SP2",
+        pvroot=pump2_pvroot,
+        readpv=f"{pump2_pvroot}FilledVolume",
+        writepv=f"{pump2_pvroot}FillVol-SP",
+        precision=0.005,
+        home_warning_msg="Please make sure syringes are removed before homing",
+    ),
+)
