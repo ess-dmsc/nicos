cd8b028242a09925a1f3cd5bab1d9a62b6cfbe86
George O'Neill <george.oneill@ess.eu>
2026-09-15 13:50:14 +0200

    better fix

--- nicos_ess/devices/datasinks/file_writer.py
+++ nicos_ess/devices/datasinks/file_writer.py
@@ -711,7 +711,7 @@ class FileWriterControlSink(Device):
             job_to_replay.start_time,
             job_to_replay.stop_time,
             job_number,
-            message.job_id
+            self._controller._generate_uuid(file_num),
         )
 
     def doShutdown(self):
